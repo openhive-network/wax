c2e10c709581e872957f2aab58ec6e750910f105
kmochocki <kmochocki@syncad.com>
2023-07-14 14:33:54 +0000

    Refactor existing endpoints to consitent one

--- cpython_interface.cpp
+++ cpython_interface.cpp
@@ -1,6 +1,5 @@
 #include "cpython_interface.hpp"
 #include "hive/libraries/chain/include/hive/chain/util/manabar.hpp"
-#include "hive/libraries/fc/include/fc/safe.hpp"
 
 #include <hive/protocol/types.hpp>
 #include <hive/protocol/operations.hpp>
@@ -54,63 +53,53 @@ namespace cpp
     return _result;
   }
 
+  hive::protocol::transaction get_transaction(const std::string& trx)
+  {
+    return fc::json::from_string( trx ).as<hive::protocol::transaction>();
+  }
+
   result protocol::cpp_validate_operation( const std::string& operation )
   {
-    auto impl = [&]( result& )
+    return method_wrapper([&]( result& )
     {
       fc::variant _v = fc::json::from_string( operation );
       hive::protocol::operation _operation = _v.as<hive::protocol::operation>();
 
       validate_visitor _visitor;
       _operation.visit( _visitor );
-    };
-
-    return method_wrapper( impl );
+    });
   };
 
   result protocol::cpp_validate_transaction( const std::string& transaction )
   {
-    auto impl = [&]( result& )
+    return method_wrapper([&]( result& )
     {
-      fc::variant _v = fc::json::from_string( transaction );
-      hive::protocol::transaction _transaction = _v.as<hive::protocol::transaction>();
-
-      _transaction.validate();
-    };
-
-    return method_wrapper( impl );
+      get_transaction(transaction).validate();
+    });
   }
 
   result protocol::cpp_calculate_digest( const std::string& transaction, const std::string& chain_id )
   {
-    auto impl = [&]( result& _result )
+    return method_wrapper([&]( result& _result )
     {
-      const auto _transaction = fc::json::from_string( transaction ).as<hive::protocol::transaction>();
-
+      const auto _transaction = get_transaction(transaction);
       const auto _digest = _transaction.sig_digest( hive::protocol::chain_id_type( chain_id ), hive::protocol::pack_type::hf26 );
 
       _result.content = _digest.str();
-    };
-
-    return method_wrapper( impl );
+    });
   }
 
   result protocol::cpp_serialize_transaction( const std::string& transaction )
   {
-    auto impl = [&]( result& _result )
+    return method_wrapper([&]( result& _result )
     {
       hive::protocol::serialization_mode_controller::mode_guard guard( hive::protocol::transaction_serialization_type::hf26 );
       hive::protocol::serialization_mode_controller::set_pack( hive::protocol::transaction_serialization_type::hf26 );
 
-      fc::variant _v = fc::json::from_string( transaction );
-      hive::protocol::transaction _transaction = _v.as<hive::protocol::transaction>();
-
-      auto _packed = fc::to_hex( fc::raw::pack_to_vector( _transaction ) );
-
+      const auto _transaction = get_transaction(transaction);
+      const auto _packed = fc::to_hex( fc::raw::pack_to_vector( _transaction ) );
       _result.content = std::string( _packed.data(), _packed.size() );
-    };
-
-    return method_wrapper( impl );
+    });
   }
 
   result protocol::cpp_generate_private_key()
