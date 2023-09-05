514a4a784d17f91c718ebe71e66e501081b63063
mtyszczak <mateusz.tyszczak@gmail.com>
2023-09-05 21:55:07 +0200

    Add suppport for publishing to multiple npm registries

--- scripts/bump_npm_version.sh
+++ scripts/bump_npm_version.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+PUBLISH_TOKEN="${1}"
+# @hive
+SCOPE="${2:-@hiveio}"
+# gitlab.syncad.com/api/v4/projects/419/packages/npm/
+REGISTRY_URL="${3:-registry.npmjs.org/}"
+
 git fetch --tags
 
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -16,4 +22,12 @@ else
   NEW_VERSION="${TAG}-dev.${SHORT_HASH}"
 fi
 
-jq ".version = \"$NEW_VERSION\"" "${CI_PROJECT_DIR}/package.json.template" > "${CI_PROJECT_DIR}/package.json"
+echo> "${CI_PROJECT_DIR}/.npmrc"
+
+if [ "$REGISTRY_URL" != "registry.npmjs.org/" ]; then
+  echo "${SCOPE}:registry=https://${REGISTRY_URL}" >> "${CI_PROJECT_DIR}/.npmrc"
+fi
+
+echo "//${REGISTRY_URL}:_authToken=\"${PUBLISH_TOKEN}\"" >> "${CI_PROJECT_DIR}/.npmrc"
+
+jq ".name = \"${SCOPE}/wax\" | .version = \"$NEW_VERSION\"" "${CI_PROJECT_DIR}/package.json.template" > "${CI_PROJECT_DIR}/package.json"
