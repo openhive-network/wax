8a65c403b6e3daa59d171b0cb8166a86adf8de4c
mtyszczak <mateusz.tyszczak@gmail.com>
2023-09-09 01:26:11 +0200

    Fix examples compiling

--- wasm/lib/protocol.ts
+++ wasm/lib/protocol.ts
@@ -1,13 +1,13 @@
 // Exports only the prootbuf definitions
 
 // Transaction types
-export { operation, transaction } from "./proto/transaction";
+export { operation, transaction } from "./proto/transaction.js";
 
 // Other types
-export { asset } from "./proto/asset";
-export { authority } from "./proto/authority";
+export { asset } from "./proto/asset.js";
+export { authority } from "./proto/authority.js";
 
 // Operations
-export { comment } from "./proto/comment";
-export { vote } from "./proto/vote";
-export { limit_order_cancel } from "./proto/limit_order_cancel";
+export { comment } from "./proto/comment.js";
+export { vote } from "./proto/vote.js";
+export { limit_order_cancel } from "./proto/limit_order_cancel.js";
