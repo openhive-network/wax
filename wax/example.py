1f03ccf4a354c66721976027cb756a6d505699e3
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-09-10 00:17:14 +0200

    Working visitor with example.

---
+++ wax/example.py
@@ -0,0 +1,27 @@
+from google.protobuf.json_format import ParseDict
+from proto import transaction_pb2
+from visitor import MyOperationVisitor
+
+tx_json = {
+    "operations": [
+        {"vote": {"voter": "Alice", "author": "Bob", "permlink": "/", "weight": 11}},
+        {"limit_order_cancel": {"order": "orderabc", "orderid": 5}},
+        {
+            "comment": {
+                "parent_permlink": "/",
+                "parent_author": "",
+                "author": "alice",
+                "permlink": "/",
+                "title": "Best comment",
+                "body": "<span>comment</span>",
+                "json_metadata": "{}",
+            }
+        },
+    ]
+}
+
+if __name__ == "__main__":
+    tx = ParseDict(tx_json, transaction_pb2.transaction())
+    visit = MyOperationVisitor()
+    for op in tx.operations:
+      visit.accept(op)
