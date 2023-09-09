e30eeb0f687fb16cf3bc2b1af3c90406ea2771dc
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-09-09 21:33:54 +0200

    Add dependencies and compile_proto_python.sh .

---
+++ scripts/compile_proto_python.sh
@@ -0,0 +1,19 @@
+
+#!/usr/bin/env sh
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+PROJECT_DIR="${SCRIPTPATH}/.."
+PROTO_DIR="${PROJECT_DIR}/hive/libraries/protocol/proto"
+
+OUTPUT_DIR="${PROJECT_DIR}/wasm/lib/proto/python"
+
+if [ -d "${OUTPUT_DIR}" ]; then
+  rm -r "${OUTPUT_DIR}"
+fi
+
+mkdir -p "${OUTPUT_DIR}"
+
+python -m grpc_tools.protoc --python_out="${OUTPUT_DIR}" --mypy_out="${OUTPUT_DIR}" -I="${PROTO_DIR}" "$PROTO_DIR"/*.proto
+
+echo "Files generated into the '${OUTPUT_DIR}' folder."
