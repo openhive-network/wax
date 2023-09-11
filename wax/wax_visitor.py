e79b83d8c8b04660fd88c548968aaddf1d6785a1
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-09-11 11:18:08 +0200

    Use poetry protoc plugin.

--- wax/wax_visitor.py
+++ wax/wax_visitor.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from wax.proto import comment_pb2, limit_order_cancel_pb2, vote_pb2
+
 
 class OperationVisitor(ABC):
     def accept(self, operation):
@@ -9,24 +11,24 @@ class OperationVisitor(ABC):
             method(operation)
 
     @abstractmethod
-    def limit_order_cancel(self, op):
+    def limit_order_cancel(self, op : limit_order_cancel_pb2.limit_order_cancel):
         pass
 
     @abstractmethod
-    def vote(self, op):
+    def vote(self, op : vote_pb2.vote):
         pass
 
     @abstractmethod
-    def comment(self, op):
+    def comment(self, op : comment_pb2):
         pass
     
 
 class MyOperationVisitor(OperationVisitor):
-  def limit_order_cancel(self, op):
+  def limit_order_cancel(self, op : limit_order_cancel_pb2.limit_order_cancel):
       print(f"Handling limit_order_cancel operation: {op}")
 
-  def vote(self, op):
+  def vote(self, op : vote_pb2.vote) -> None:
       print(f"Handling vote operation: {op}")
 
-  def comment(self, op):
+  def comment(self, op : comment_pb2.comment) -> None:
       print(f"Handling comment operation: {op}")
