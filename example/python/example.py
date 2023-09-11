2709d0abc9b29b320f75e1ed3471006ed8227da5
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-09-11 12:36:19 +0200

    Add visitor test of protobuf.

--- example/python/example.py
+++ example/python/example.py
@@ -1,6 +1,7 @@
-from google.protobuf.json_format import ParseDict
-from wax.proto import transaction_pb2
-from wax.wax_visitor import MyOperationVisitor
+from google.protobuf.json_format import MessageToDict, ParseDict
+
+from wax.proto import comment_pb2, limit_order_cancel_pb2, transaction_pb2, vote_pb2
+from wax.wax_visitor import OperationVisitor
 
 tx_json = {
     "operations": [
@@ -20,8 +21,33 @@ tx_json = {
     ]
 }
 
+
+class MyOperationVisitor(OperationVisitor):
+    def limit_order_cancel(self, op: limit_order_cancel_pb2.limit_order_cancel):
+        print(f"Handling limit_order_cancel operation:\n{op}")
+        assert op.order == "orderabc"
+        assert op.orderid == 5
+
+    def vote(self, op: vote_pb2.vote) -> None:
+        print(f"Handling vote operation:\n{op}")
+        assert op.voter == "Alice"
+        assert op.author == "Bob"
+        assert op.permlink == "/"
+        assert op.weight == 11
+
+    def comment(self, op: comment_pb2.comment) -> None:
+        print(f"Handling comment operation:\n{op}")
+        assert op.parent_permlink == "/"
+        assert op.parent_author == ""
+        assert op.author == "alice"
+        assert op.permlink == "/"
+        assert op.title == "Best comment"
+        assert op.body == "<span>comment</span>"
+        assert op.json_metadata == "{}"
+
+
 if __name__ == "__main__":
     tx = ParseDict(tx_json, transaction_pb2.transaction())
     visit = MyOperationVisitor()
     for op in tx.operations:
-      visit.accept(op)
+        visit.accept(op)
