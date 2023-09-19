e6429890dfe9c9f107d78d5c0dd27fe26e653ec3
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-09-19 15:01:23 +0200

    Fix wax python visitor.

--- wax/wax_visitor.py
+++ wax/wax_visitor.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
 
-from wax.proto import comment_pb2, limit_order_cancel_pb2, transaction_pb2, vote_pb2, recurrent_transfer_pb2
+from wax.proto import (
+    comment_pb2,
+    limit_order_cancel_pb2,
+    recurrent_transfer_pb2,
+    transaction_pb2,
+    vote_pb2,
+)
 
 
 class OperationVisitor(ABC):
