15d24f9522d7577692e08e83cc334aa794071d80
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-09-20 13:08:12 +0200

    Use ci-base-image docker image, and add possibility to run build locally or in docker.

--- scripts/build_wax_python.sh
+++ scripts/build_wax_python.sh
@@ -5,6 +5,9 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
+DIRECT_EXECUTION=${1:-0}
+
+if [ ${DIRECT_EXECUTION} -eq 0 ]; then
 COMMIT_HASH=$(git rev-parse --short HEAD)
 IMAGE_NAME="wax-python-builder:"${COMMIT_HASH}
 
@@ -13,36 +16,39 @@ USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 WORKDIR=/home/${USER_NAME}
 
-echo "Cleaning up."
-if [ -d "${PROJECT_DIR}/dist" ] ; then
-    echo "Removing '${PROJECT_DIR}/dist'."
-    rm -rf "${PROJECT_DIR}/dist"
-fi
-
-if [ -d "${PROJECT_DIR}/build" ] ; then
-    echo "Removing '${PROJECT_DIR}/build'."
-    rm -rf "${PROJECT_DIR}/build"
-fi
-
-if [ -d "${PROJECT_DIR}/.build" ] ; then 
-    echo "Removing '${PROJECT_DIR}/.build'."
-    rm -rf "${PROJECT_DIR}/.build"
-fi
-
 echo "Create wax python builder."
 docker build \
        -f ${PROJECT_DIR}/docker/wax-python-builder.dockerfile \
-     --build-arg USER_NAME=${USER_NAME} \
-     --build-arg USER_ID=${USER_ID}  \
-     --build-arg GROUP_ID=${GROUP_ID} \
+       --build-arg USER_NAME=${USER_NAME} \
+       --build-arg USER_ID=${USER_ID} \
+       --build-arg GROUP_ID=${GROUP_ID} \
      -t ${IMAGE_NAME} \
      ${PROJECT_DIR}
 
-echo "Create proto files."
-docker run -v ${PROJECT_DIR}:${WORKDIR}/wax ${IMAGE_NAME} bash -c "${WORKDIR}/wax/scripts/compile_proto_python.sh"
+docker run -v ${PROJECT_DIR}:${WORKDIR}/wax ${IMAGE_NAME} bash -c "${WORKDIR}/wax/scripts/build_wax_python.sh 1"
 
-echo "Build wax wheel package."
-docker run -v ${PROJECT_DIR}:${WORKDIR}/wax ${IMAGE_NAME} poetry -C ${WORKDIR}/wax build
+else
+  echo "Cleaning up."
+  DIRS=("dist" "build" ".build")
 
-echo "List dist directory."
-ls ${PROJECT_DIR}/dist
+  for directory in "${DIRS[@]}"; do
+      dir_path="${PROJECT_DIR}/${directory}"
+      
+      if [ -d "$dir_path" ]; then
+          echo "Removing '$dir_path'."
+          rm -rf "$dir_path"
+      fi
+      
+      mkdir "$dir_path"
+  done
+
+  cd ${PROJECT_DIR}/wax
+  echo "Create proto files."
+  ${PROJECT_DIR}/scripts/compile_proto_python.sh
+
+  echo "Build wax wheel package."
+  poetry build
+
+  echo "List dist directory."
+  ls ${PROJECT_DIR}/dist
+fi
