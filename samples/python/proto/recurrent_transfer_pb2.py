a1d10df60ebc55b973f789b78c98e78034c7bbaf
Marek <marek@syncad.com>
2023-09-21 12:47:47 +0000

    Change python generated patterns

--- samples/python/proto/recurrent_transfer_pb2.py
+++ samples/python/proto/recurrent_transfer_pb2.py
@@ -15,7 +15,7 @@ import future_extensions_pb2 as future__extensions__pb2
 import asset_pb2 as asset__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18recurrent_transfer.proto\x12\x15hive.protocol.buffers\x1a\x17\x66uture_extensions.proto\x1a\x0b\x61sset.proto\"6\n\x1arecurrent_transfer_pair_id\x12\x18\n\x07pair_id\x18\x01 \x02(\rR\x07pair_id\"\xd9\x01\n\x1crecurrent_transfer_extension\x12\x37\n\x06void_t\x18\x01 \x01(\x0b\x32\x1d.hive.protocol.buffers.void_tH\x00R\x06void_t\x12s\n\x1arecurrent_transfer_pair_id\x18\x02 \x01(\x0b\x32\x31.hive.protocol.buffers.recurrent_transfer_pair_idH\x00R\x1arecurrent_transfer_pair_idB\x0b\n\textension\"\xe2\x01\n\x12recurrent_transfer\x12\x13\n\x05\x66rom_\x18\x01 \x02(\tR\x04\x66rom\x12\n\n\x02to\x18\x02 \x02(\t\x12,\n\x06\x61mount\x18\x03 \x02(\x0b\x32\x1c.hive.protocol.buffers.asset\x12\x0c\n\x04memo\x18\x04 \x02(\t\x12\x12\n\nrecurrence\x18\x05 \x02(\r\x12\x12\n\nexecutions\x18\x06 \x02(\r\x12G\n\nextensions\x18\x07 \x03(\x0b\x32\x33.hive.protocol.buffers.recurrent_transfer_extension')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18recurrent_transfer.proto\x12\x15hive.protocol.buffers\x1a\x17\x66uture_extensions.proto\x1a\x0b\x61sset.proto\"6\n\x1arecurrent_transfer_pair_id\x12\x18\n\x07pair_id\x18\x01 \x02(\rR\x07pair_id\"\xd9\x01\n\x1crecurrent_transfer_extension\x12\x37\n\x06void_t\x18\x01 \x01(\x0b\x32\x1d.hive.protocol.buffers.void_tH\x00R\x06void_t\x12s\n\x1arecurrent_transfer_pair_id\x18\x02 \x01(\x0b\x32\x31.hive.protocol.buffers.recurrent_transfer_pair_idH\x00R\x1arecurrent_transfer_pair_idB\x0b\n\textension\"\xf5\x01\n\x12recurrent_transfer\x12\x1a\n\x0c\x66rom_account\x18\x01 \x02(\tR\x04\x66rom\x12\x16\n\nto_account\x18\x02 \x02(\tR\x02to\x12,\n\x06\x61mount\x18\x03 \x02(\x0b\x32\x1c.hive.protocol.buffers.asset\x12\x0c\n\x04memo\x18\x04 \x02(\t\x12\x12\n\nrecurrence\x18\x05 \x02(\r\x12\x12\n\nexecutions\x18\x06 \x02(\r\x12G\n\nextensions\x18\x07 \x03(\x0b\x32\x33.hive.protocol.buffers.recurrent_transfer_extension')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -28,5 +28,5 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['_RECURRENT_TRANSFER_EXTENSION']._serialized_start=146
   _globals['_RECURRENT_TRANSFER_EXTENSION']._serialized_end=363
   _globals['_RECURRENT_TRANSFER']._serialized_start=366
-  _globals['_RECURRENT_TRANSFER']._serialized_end=592
+  _globals['_RECURRENT_TRANSFER']._serialized_end=611
 # @@protoc_insertion_point(module_scope)
