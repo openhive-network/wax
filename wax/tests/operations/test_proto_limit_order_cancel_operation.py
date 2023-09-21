af7c024fa07e07f9afe260154e8eb50804d09e11
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-09-21 12:57:46 +0200

    Add tests and pytest.

---
+++ wax/tests/operations/test_proto_limit_order_cancel_operation.py
@@ -0,0 +1,15 @@
+from utils.checkers import check_operations
+
+from wax.proto import limit_order_cancel_pb2, transaction_pb2
+
+
+def test_limit_order_cancel_operation():
+    limit_order_cancel: limit_order_cancel_pb2.limit_order_cancel = (
+        limit_order_cancel_pb2.limit_order_cancel(orderid=1, order="adm")
+    )
+
+    limit_order_cancel_operation: transaction_pb2.operation = transaction_pb2.operation(
+        limit_order_cancel=limit_order_cancel
+    )
+
+    check_operations(limit_order_cancel_operation)
