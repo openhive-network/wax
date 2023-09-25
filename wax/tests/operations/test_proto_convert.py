c215830939128657c7e238c94cce218ec4ef1b2e
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-09-25 14:57:04 +0200

    Add python test for proto transfer_to_vesting, withdraw_vesting, limit_order_create, feed_publish, convert, account_update, account_witness_vote, account_witness_proxy, custom, witness_block_approve

---
+++ wax/tests/operations/test_proto_convert.py
@@ -0,0 +1,31 @@
+# convert_operation = {
+#   "type": "convert_operation",
+#   "value": {
+#     "owner": "summon",
+#     "requestid": 1467592156,
+#     "amount": {
+#       "amount": "5000",
+#       "precision": 3,
+#       "nai": "@@000000013"
+#     }
+#   }
+# }
+
+from utils.checkers import check_operations
+
+from wax.proto import asset_pb2, convert_pb2, transaction_pb2
+
+
+def test_convert():
+    amount: asset_pb2.asset = asset_pb2.asset(
+        nai="@@000000013", precision=3, amount="5000"
+    )
+
+    conver: convert_pb2.convert = convert_pb2.convert(
+        owner="summon", requestid=1467592156, amount=amount
+    )
+
+    conver_operation: transaction_pb2.operation = transaction_pb2.operation(
+        convert=conver
+    )
+    check_operations(conver_operation)
---
+++ wax/tests/operations/test_proto_withdraw_vesting.py
@@ -0,0 +1,33 @@
+# withdraw_vesting_operation={
+#   "type": "withdraw_vesting_operation",
+#   "value": {
+#     "account": "steemit",
+#     "vesting_shares": {
+#       "amount": "200000000000",
+#       "precision": 6,
+#       "nai": "@@000000037"
+#     }
+#   }
+# }
+
+from utils.checkers import check_operations
+
+from wax.proto import asset_pb2, transaction_pb2, withdraw_vesting_pb2
+
+
+def test_withdraw_vesting():
+    vesting_shares: asset_pb2.asset = asset_pb2.asset(
+        nai="@@000000037", precision=6, amount="200000000000"
+    )
+
+    withdraw_vesting: withdraw_vesting_pb2.withdraw_vesting = (
+        withdraw_vesting_pb2.withdraw_vesting(
+            account="steemit", vesting_shares=vesting_shares
+        )
+    )
+
+    withdraw_vesting_operation: transaction_pb2.operation = transaction_pb2.operation(
+        withdraw_vesting=withdraw_vesting
+    )
+
+    check_operations(withdraw_vesting_operation)
