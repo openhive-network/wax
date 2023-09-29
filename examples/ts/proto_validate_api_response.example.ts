0b7e287de09a1fbd54b55d99cc3a635b6a9f214c
mtyszczak <mateusz.tyszczak@gmail.com>
2023-09-29 18:50:45 +0200

    Allow example functions to be used as evaluates in playwright

--- examples/ts/proto_validate_api_response.example.ts
+++ examples/ts/proto_validate_api_response.example.ts
@@ -1,4 +1,9 @@
-import MainModule from "@hive/wax";
+import { fileURLToPath } from 'url';
+import process from 'process';
+
+export const evaluate = async() => {
+
+const { default: MainModule } = await import("@hive/wax");
 
 const apiData = JSON.stringify({
   "ref_block_num": 19260,
@@ -21,9 +26,16 @@ const apiData = JSON.stringify({
   ]
 });
 
-(async () => {
-  const provider = await MainModule();
-  const protoInstance = new provider.proto_protocol();
+const provider = await MainModule();
+const protoInstance = new provider.proto_protocol();
 
-  protoInstance.cpp_validate_transaction(apiData);
-})();
+const protoData = protoInstance.cpp_api_to_proto(apiData);
+console.assert(protoData.value === provider.error_code.ok);
+
+const result = protoInstance.cpp_validate_transaction(protoData.content);
+console.assert(result.value === provider.error_code.ok);
+};
+
+// Run evaluate function when running from the console, instead of importing a module
+if(process.argv[1] === fileURLToPath(import.meta.url))
+  evaluate();
