19aaa075e9f8e3ecc5e3f73ca0245231f0143b33
Bartek Wrona <wrona@syncad.com>
2023-10-05 23:41:00 +0200

    Defined common implementation class: protobuf_protocol_impl

---
+++ core/protobuf_protocol_impl.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "core/types.hpp"
+
+#include <string>
+
+namespace cpp {
+
+/** Common implementation of protobuf_protocol interface, next exposed to other languages 
+*   It provides Hive protocol functionality operating on Protobuf specific JSON format.
+*/
+template <class FoundationProvider>
+class proto_protocol_impl : public FoundationProvider
+{
+public:
+  result cpp_validate_operation(const std::string& operation);
+  result cpp_validate_transaction(const std::string& transaction);
+  result cpp_calculate_transaction_id(const std::string& transaction);
+  result cpp_calculate_sig_digest(const std::string& transaction, const std::string& chain_id);
+  result cpp_serialize_transaction(const std::string& transaction);
+
+  // TODO: Implement block bi-directional protobuf JSON conversion
+  result cpp_proto_to_api(const std::string& operation_or_tx);
+  result cpp_api_to_proto(const std::string& operation_or_tx);
+};
+
+} /// namespace cpp
