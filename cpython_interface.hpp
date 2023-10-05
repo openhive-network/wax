879ed1f74f49055ed6f6f343fd97e77062752ad3
Bartek Wrona <wrona@syncad.com>
2023-10-05 21:32:20 +0200

    foundation class moved to dedicated header file: core/foundation.hpp

--- cpython_interface.hpp
+++ cpython_interface.hpp
@@ -3,6 +3,8 @@
 #include<string>
 #include<functional>
 
+#include "core/foundation.hpp"
+
 namespace cpp
 {
   enum error_code { fail = 0, ok = 1 };
@@ -22,25 +24,6 @@ namespace cpp
     std::string amount;
     uint32_t precision; // XXX: is uint64_t actually
     std::string nai;
-  };
-
-  ///  Common base providing functionality independent on JSON format being used for parameter transport
-  class foundation
-  {
-  public:
-    result cpp_calculate_public_key(const std::string& wif);
-    result cpp_generate_private_key();
-
-    json_asset cpp_general_asset(const uint32_t asset_num, const int64_t amount)const;
-    json_asset cpp_hive(const int64_t amount)const;
-    json_asset cpp_hbd(const int64_t amount)const;
-    json_asset cpp_vests(const int64_t amount)const;
-
-    result cpp_calculate_manabar_full_regeneration_time(const int32_t now, const int64_t max_mana, const int64_t current_mana, const uint32_t last_update_time);
-    result cpp_calculate_current_manabar_value(const int32_t now, const int64_t max_mana, const int64_t current_mana, const uint32_t last_update_time);
-
-  protected:
-    ~foundation() = default; /// use this iface only through derived ones
   };
 
   ///  Interface providing Hive protocol functionality operating on Hive native JSON format.
