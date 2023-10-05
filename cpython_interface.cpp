03821c5cbaf9a0d3d40d6cf0040958cdcbe2dd3c
Bartek Wrona <wrona@syncad.com>
2023-10-05 21:42:31 +0200

    method_wrapper implementation moved to the core/utils.hpp

--- cpython_interface.cpp
+++ cpython_interface.cpp
@@ -30,32 +30,6 @@ namespace cpp
       x.validate();
     }
   };
-  using callback = std::function<void(result&)>;
-
-  result method_wrapper(callback&& method)
-  {
-    result _result;
-
-    try
-    {
-      method( _result );
-      _result.value = ok;
-    }
-    catch( fc::exception& e )
-    {
-      _result.exception_message = e.to_detail_string();
-    }
-    catch( const std::exception& e )
-    {
-      _result.exception_message = e.what();
-    }
-    catch(...)
-    {
-      _result.exception_message = "Unknown exception.";
-    }
-
-    return _result;
-  }
 
   hive::protocol::transaction get_transaction(const std::string& trx)
   {
