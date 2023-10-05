02325ff3c600d8955366d6efa4af023f2b75696c
Bartek Wrona <wrona@syncad.com>
2023-10-06 00:41:54 +0200

    Cleaned up unused header includes and log function.

--- cpython_interface.cpp
+++ cpython_interface.cpp
@@ -1,23 +1,6 @@
 #include "cpython_interface.hpp"
 
-#include <hive/protocol/types.hpp>
-
-#include <boost/preprocessor/variadic/to_seq.hpp>
-#include <boost/preprocessor/seq/enum.hpp>
-#include <boost/preprocessor/seq/transform.hpp>
-
-#include <fc/io/json.hpp>
-
-#include <iostream>
-#include <string>
-
-
 namespace cpp
 {
-  void log( const std::string& message )
-  {
-    std::cout<< message << std::endl;
-  }
-
 
 } // namespace cpp
