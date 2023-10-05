efbcbb8a41d88053ec3f6d547b29798fca6ecfb4
Bartek Wrona <wrona@syncad.com>
2023-10-05 23:42:29 +0200

    Python specific protocol and proto_protocol defined using template instantiations of dedicated impl classes.

--- cpython_interface.hpp
+++ cpython_interface.hpp
@@ -2,35 +2,16 @@
 
 #include<string>
 
-
 #include "core/foundation.hpp"
+#include "core/protocol_impl.hpp"
+#include "core/protobuf_protocol_impl.hpp"
 
 namespace cpp
 {
  
   ///  Interface providing Hive protocol functionality operating on Hive native JSON format.
-  class protocol : public foundation
-  {
-    public:
-      result cpp_validate_operation( const std::string& operation );
-      result cpp_validate_transaction( const std::string& transaction );
-      result cpp_calculate_transaction_id( const std::string& transaction );
-      result cpp_calculate_sig_digest( const std::string& transaction, const std::string& chain_id );
-      result cpp_serialize_transaction( const std::string& transaction );
-  };
+  class protocol : public protocol_impl<foundation> {};
 
   ///  Interface providing Hive protocol functionality operating on Protobuf specific JSON format.
-  class proto_protocol : public foundation
-  {
-    public:
-      result cpp_validate_operation( const std::string& operation );
-      result cpp_validate_transaction( const std::string& transaction );
-      result cpp_calculate_transaction_id( const std::string& transaction );
-      result cpp_calculate_sig_digest( const std::string& transaction, const std::string& chain_id );
-      result cpp_serialize_transaction( const std::string& transaction );
-
-      // TODO: Implement block bi-directional protobuf JSON conversion
-      result cpp_proto_to_api( const std::string& operation_or_tx );
-      result cpp_api_to_proto( const std::string& operation_or_tx );
-  };
+  class proto_protocol : public proto_protocol_impl<foundation> {};
 }
