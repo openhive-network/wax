530d2cdad6d862b9728984df6fdba6ead342e15b
Marek <marek@syncad.com>
2023-10-06 12:07:56 +0000

    Add python test for wax.calculate_proto_transaction_id method

---
+++ wax/tests/proto-protocol/test_calculate_proto_transaction_id.py
@@ -0,0 +1,20 @@
+import json
+
+from utils.refs import PROTO_REF_TRANSACTION, API_REF_TRANSACTION
+
+from wax import calculate_proto_transaction_id
+
+def test_calculate_proto_transaction_id():
+    tx_str = json.dumps(PROTO_REF_TRANSACTION)
+    result = calculate_proto_transaction_id(tx_str.encode())
+    assert result.status == result.status.ok
+    assert result.exception_message == b''
+    assert result.result == b'4491c7a6362e71cca31e256f69af503e0abc5d3d'
+
+    # Negative test
+    tx_str = json.dumps(API_REF_TRANSACTION)
+    result = calculate_proto_transaction_id(tx_str.encode())
+    assert result.status == result.status.fail
+    assert result.exception_message == (
+        b'10 assert_exception: Assert Exception\nop.get_object()[key].is_object()'
+        b'\nOperation should contain the body\n    {}\n    cpython_interface.cpp:92 parse_proto_operation')
