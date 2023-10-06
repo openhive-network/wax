994d4284142b7eafb3b600c78a391c02f8d82783
Marek <marek@syncad.com>
2023-10-06 15:06:31 +0000

    Add python test for wax.calculate_sig_digest method

---
+++ wax/tests/protocol/test_calculate_sig_digest.py
@@ -0,0 +1,19 @@
+import json
+
+from utils.refs import API_REF_TRANSACTION, PROTO_REF_TRANSACTION
+
+from wax import calculate_sig_digest
+
+def test_calculate_sig_digest():
+    tx_str = json.dumps(API_REF_TRANSACTION)
+    result = calculate_sig_digest(tx_str.encode(), b'beeab0de00000000000000000000000000000000000000000000000000000000')
+    assert result.status == result.status.ok
+    assert result.exception_message == b''
+    assert result.result == b'b31ff450905ad705ed0d7fd5e270c3685442203e15e1b1e7d5e94b35dcdc1693'
+
+    # Negative test
+    tx_str = json.dumps(PROTO_REF_TRANSACTION)
+    result = calculate_sig_digest(tx_str.encode(), b'beeab0de00000000000000000000000000000000000000000000000000000000')
+    assert result.status == result.status.fail
+    assert result.exception_message == (
+        b'10 assert_exception: Assert Exception\nv_object.contains( "type" )\nType field doesn\'t exist.\n    {}\n    static_variant.hpp:465 from_variant')
