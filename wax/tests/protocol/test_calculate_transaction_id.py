2405b55e074f29b2e83ba2483af4627d4876a99d
Marek <marek@syncad.com>
2023-10-06 15:05:57 +0000

    Add python test for wax.calculate_transaction_id method

---
+++ wax/tests/protocol/test_calculate_transaction_id.py
@@ -0,0 +1,19 @@
+import json
+
+from utils.refs import API_REF_TRANSACTION, PROTO_REF_TRANSACTION
+
+from wax import calculate_transaction_id
+
+def test_calculate_proto_transaction_id():
+    tx_str = json.dumps(API_REF_TRANSACTION)
+    result = calculate_transaction_id(tx_str.encode())
+    assert result.status == result.status.ok
+    assert result.exception_message == b''
+    assert result.result == b'4491c7a6362e71cca31e256f69af503e0abc5d3d'
+
+    # Negative test
+    tx_str = json.dumps(PROTO_REF_TRANSACTION)
+    result = calculate_transaction_id(tx_str.encode())
+    assert result.status == result.status.fail
+    assert result.exception_message == (
+        b'10 assert_exception: Assert Exception\nv_object.contains( "type" )\nType field doesn\'t exist.\n    {}\n    static_variant.hpp:465 from_variant')
