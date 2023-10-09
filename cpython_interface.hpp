fc233d426d5e325c0d75d39b8e6b79bcb906f324
Bartek Wrona <wrona@syncad.com>
2023-10-09 22:25:40 +0200

    protocol class inherits foundation class to reuse basic functionality

--- cpython_interface.hpp
+++ cpython_interface.hpp
@@ -43,30 +43,15 @@ namespace cpp
     ~foundation() = default; /// use this iface only through derived ones
   };
 
-
-  class protocol
+  ///  Interface providing Hive protocol functionality operating on Hive native JSON format.
+  class protocol : public foundation
   {
-    protected:
-
-      using callback = std::function<void(result&)>;
-
-      result method_wrapper( callback&& method );
-
     public:
-
       result cpp_validate_operation( const std::string& operation );
       result cpp_validate_transaction( const std::string& transaction );
       result cpp_calculate_transaction_id( const std::string& transaction );
       result cpp_calculate_sig_digest( const std::string& transaction, const std::string& chain_id );
       result cpp_serialize_transaction( const std::string& transaction );
-      result cpp_calculate_public_key( const std::string& wif );
-      result cpp_generate_private_key();
-      result cpp_calculate_manabar_full_regeneration_time( const int32_t now, const int64_t max_mana, const int64_t current_mana, const uint32_t last_update_time );
-      result cpp_calculate_current_manabar_value( const int32_t now, const int64_t max_mana, const int64_t current_mana, const uint32_t last_update_time );
-      json_asset cpp_general_asset(const uint32_t asset_num, const int64_t amount)const;
-      json_asset cpp_hive(const int64_t amount)const;
-      json_asset cpp_hbd(const int64_t amount)const;
-      json_asset cpp_vests(const int64_t amount)const;
   };
 
   class proto_protocol : public protocol
