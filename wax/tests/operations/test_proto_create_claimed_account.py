fc662824e8a3d491d6855e5217468346a6916aaa
Marek <marek@syncad.com>
2023-10-10 13:05:08 +0000

    Add python test for create_claimed_account operation

---
+++ wax/tests/operations/test_proto_create_claimed_account.py
@@ -0,0 +1,43 @@
+from utils.checkers import check_operations, check_transaction
+
+from wax.proto import (
+    asset_pb2,
+    create_claimed_account_pb2,
+    operation_pb2,
+    transaction_pb2,
+    future_extensions_pb2,
+    authority_pb2
+)
+
+def test_create_claimed_account():
+    extension: future_extensions_pb2.future_extensions = future_extensions_pb2.future_extensions()
+    asset: asset_pb2.asset = asset_pb2.asset(
+        nai="@@000000021", precision=3, amount="10"
+    )
+    authority: authority_pb2.authority = authority_pb2.authority(
+        weight_threshold=1,
+        account_auths={"STM6FATHLohxTN8RWWkU9ZZwVywXo6MEDjHHui1jEBYkG2tTdvMYo": 1},
+        key_auths={"STM76EQNV2RTA6yF9TnBvGSV71mW7eW36MM7XQp24JxdoArTfKA76": 1}
+    )
+    create_claimed_account: create_claimed_account_pb2.create_claimed_account = create_claimed_account_pb2.create_claimed_account(
+        creator="creator",
+        new_account_name="new_name",
+        owner=authority,
+        active=authority,
+        posting=authority,
+        memo_key="STM6FATHLohxTN8RWWkU9ZZwVywXo6MEDjHHui1jEBYkG2tTdvMYo",
+        json_metadata="{}",
+        extensions=[]
+    )
+
+    create_claimed_account_operation: operation_pb2.operation = operation_pb2.operation(
+        create_claimed_account=create_claimed_account
+    )
+
+    check_operations(create_claimed_account_operation)
+
+    transaction: transaction_pb2.transaction = transaction_pb2.transaction(
+        operations=[create_claimed_account_operation]
+    )
+
+    check_transaction(transaction)
