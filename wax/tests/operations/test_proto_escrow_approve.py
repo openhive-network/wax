4c33fe71d4507eecf096917bb5769e9498074cec
Marek <marek@syncad.com>
2023-10-10 14:52:04 +0000

    Add python test for escrow_approve operation

---
+++ wax/tests/operations/test_proto_escrow_approve.py
@@ -0,0 +1,29 @@
+from utils.checkers import check_operations, check_transaction
+
+from wax.proto import (
+    operation_pb2,
+    transaction_pb2,
+    escrow_approve_pb2
+)
+
+def test_escrow_approve():
+    escrow_approve: escrow_approve_pb2.escrow_approve = escrow_approve_pb2.escrow_approve(
+        from_account="faddy",
+        to_account="daddy",
+        agent="agent",
+        who="who",
+        escrow_id=1,
+        approve=True
+    )
+
+    escrow_approve_operation: operation_pb2.operation = (
+        operation_pb2.operation(escrow_approve=escrow_approve)
+    )
+
+    check_operations(escrow_approve_operation)
+
+    transaction: transaction_pb2.transaction = transaction_pb2.transaction(
+        operations=[escrow_approve_operation]
+    )
+
+    check_transaction(transaction)
