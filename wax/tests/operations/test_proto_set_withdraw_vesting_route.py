abc04769b6d87d17d44c34864943be88e565681c
Marek <marek@syncad.com>
2023-10-10 15:01:39 +0000

    Add python test for set_withdraw_vesting_route operation

---
+++ wax/tests/operations/test_proto_set_withdraw_vesting_route.py
@@ -0,0 +1,29 @@
+from utils.checkers import check_operations, check_transaction
+
+from wax.proto import (
+    operation_pb2,
+    transaction_pb2,
+    set_withdraw_vesting_route_pb2
+)
+
+def test_set_withdraw_vesting_route():
+    set_withdraw_vesting_route: set_withdraw_vesting_route_pb2.set_withdraw_vesting_route = (
+        set_withdraw_vesting_route_pb2.set_withdraw_vesting_route(
+            from_account="faddy",
+            to_account="",
+            percent=10,
+            auto_vest=True
+        )
+    )
+
+    set_withdraw_vesting_route_opertaion: operation_pb2.operation = (
+        operation_pb2.operation(set_withdraw_vesting_route=set_withdraw_vesting_route)
+    )
+
+    check_operations(set_withdraw_vesting_route_opertaion)
+
+    transaction: transaction_pb2.transaction = transaction_pb2.transaction(
+        operations=[set_withdraw_vesting_route_opertaion]
+    )
+
+    check_transaction(transaction)
