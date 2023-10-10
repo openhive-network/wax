0f781e59bbd25c2c95c7d1003c4f7010b5a1044b
Marek <marek@syncad.com>
2023-10-10 13:04:24 +0000

    Add python test for comment_options operation

---
+++ wax/tests/operations/test_proto_comment_options.py
@@ -0,0 +1,36 @@
+from utils.checkers import check_operations, check_transaction
+
+from wax.proto import (
+    asset_pb2,
+    comment_options_pb2,
+    operation_pb2,
+    transaction_pb2
+)
+
+def test_comment_options():
+    beneficiary_route : comment_options_pb2.beneficiary_route_type = comment_options_pb2.beneficiary_route_type(account="account", weight=10)
+    comment_payout_beneficiaries = comment_options_pb2.comment_payout_beneficiaries(beneficiaries=[beneficiary_route])
+    asset: asset_pb2.asset = asset_pb2.asset(
+        nai="@@000000021", precision=3, amount="10"
+    )
+    comment_options: comment_options_pb2.comment_options = comment_options_pb2.comment_options(
+        author="author",
+        permlink="/",
+        max_accepted_payout=asset,
+        percent_hbd=10,
+        allow_votes=True,
+        allow_curation_rewards=True,
+        extensions=[]
+    )
+
+    comment_options_operation: operation_pb2.operation = operation_pb2.operation(
+        comment_options=comment_options
+    )
+
+    check_operations(comment_options_operation)
+
+    transaction: transaction_pb2.transaction = transaction_pb2.transaction(
+        operations=[comment_options_operation]
+    )
+
+    check_transaction(transaction)
