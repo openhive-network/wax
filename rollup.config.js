c5ac27f07c05e8802c9027ef8d7086dfbebb92df
mtyszczak <mateusz.tyszczak@gmail.com>
2023-11-07 15:34:19 +0100

    Add bundler to the project

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from 'rollup-plugin-typescript2';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'wasm/dist/index.js',
+  output: {
+    dir: 'wasm/dist/bundle',
+    format: 'es',
+    name: 'wax'
+  },
+  plugins: [
+    nodeResolve({ preferBuiltins: false }),
+    typescript({
+      rollupCommonJSResolveHack: false,
+      clean: true
+    }),
+    commonjs()
+  ]
+};
