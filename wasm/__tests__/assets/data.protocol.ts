78f721c416464f56e4fe44f1dc56810cfb26e44b
Bartek Wrona <wrona@syncad.com>
2023-11-09 23:04:02 +0100

    Defined example (mainnet) serialization sensitive transaction to have some input for further unit tests.

--- wasm/__tests__/assets/data.protocol.ts
+++ wasm/__tests__/assets/data.protocol.ts
@@ -37,3 +37,26 @@ export const transaction = JSON.stringify({
   ],
   extensions: []
 });
+
+/// See Hive mainnet block 80021416, trx_id: 7f34699e9eea49d1bcc10c88f96e38897839ece3
+export const serialization_sensitive_transaction = JSON.stringify({
+  ref_block_num: 1959,
+  ref_block_prefix: 3625727107,
+  expiration: "2023-11-09T22:01:24",
+  operations: [
+    {
+      type: "transfer_operation",
+      value: {
+        from: "oneplus7",
+        to: "kryptogames",
+        amount: {
+          amount: "300000",
+          precision: 3,
+          nai: "@@000000021"
+        },
+        memo: "Roll under 50 4d434bd943616"
+      }
+    }
+  ],
+  extensions: []
+});
