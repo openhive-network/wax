f6cb02c06b0c2e3486df0b7c7e1cec12351c7a51
Wiesław Kędzierski <wkedzierski@syncad.com>
2023-11-16 09:38:37 +0100

    Review: tests should be called the same way, regardless of the environment.

--- wax/tests/run_tests.sh
+++ wax/tests/run_tests.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [[ -v CI_PROJECT_DIR ]]; then
-pytest --junitxml="${CI_PROJECT_DIR}/wax/tests/report.xml" "${CI_PROJECT_DIR}/wax/tests/"
-else
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-poetry run pytest ${SCRIPTPATH}
-fi
+poetry run pytest --junitxml="${SCRIPTPATH}/report.xml" "${SCRIPTPATH}"
