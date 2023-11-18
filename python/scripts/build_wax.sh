c632c7eabf0a6e9ca0b48bf21d8a26947d0bde63
Bartek Wrona <wrona@syncad.com>
2023-11-18 19:22:18 +0100

    build_wax.sh script adjusted to new project structure.

--- python/scripts/build_wax.sh
+++ python/scripts/build_wax.sh
@@ -3,7 +3,7 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-PROJECT_DIR="$(dirname "$SCRIPT_DIR")/.."
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")/../python"
 
 DIRECT_EXECUTION=${1:-0}
 
@@ -16,16 +16,18 @@ USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 WORKDIR=/home/${USER_NAME}
 
+echo ${PROJECT_DIR}
+
 echo "Create wax python builder."
 docker build \
-       -f ${PROJECT_DIR}/python/docker/wax-python-builder.dockerfile \
+       -f ${PROJECT_DIR}/docker/wax-python-builder.dockerfile \
        --build-arg USER_NAME=${USER_NAME} \
        --build-arg USER_ID=${USER_ID} \
        --build-arg GROUP_ID=${GROUP_ID} \
      -t ${IMAGE_NAME} \
-     ${PROJECT_DIR}
+     ${PROJECT_DIR}/../
 
-docker run --rm -v ${PROJECT_DIR}:${WORKDIR}/wax ${IMAGE_NAME} bash -c "${WORKDIR}/wax/python/scripts/build_wax.sh 1"
+docker run --rm -v ${PROJECT_DIR}/../:${WORKDIR}/wax ${IMAGE_NAME} bash -c "${WORKDIR}/wax/python/scripts/build_wax.sh 1"
 
 else
   echo "Cleaning up."
@@ -39,16 +41,16 @@ else
           rm -rf "$dir_path"
       fi
       
-      mkdir "$dir_path"
+      mkdir -v "$dir_path"
   done
 
   cd ${PROJECT_DIR}/wax
   echo "Create proto files."
-  ${PROJECT_DIR}/python/scripts/compile_proto.sh
+  ${PROJECT_DIR}/scripts/compile_proto.sh
 
   echo "Build wax wheel package."
   poetry -C ${PROJECT_DIR} build --format wheel
 
-  echo "List dist directory."
-  ls ${PROJECT_DIR}/dist
+  echo "List dist directory: ${PROJECT_DIR}/dist"
+  ls -lA ${PROJECT_DIR}/dist
 fi
