88b7b1c40be49488cbc8123fbb4a03399cce6ec1
Bartek Wrona <wrona@syncad.com>
2023-11-19 16:08:28 +0100

    python/run_example.sh adjusted to new project structure

--- examples/python/run_example.sh
+++ examples/python/run_example.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -e
+
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-poetry run python ${SCRIPTPATH}/visitor_example.py
+
+poetry -C "${SCRIPTPATH}/../../python" run python ${SCRIPTPATH}/visitor_example.py
