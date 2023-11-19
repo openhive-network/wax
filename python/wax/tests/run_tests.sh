b28f62ddf5500d8509502e5ab6cdbca0db308dc2
Bartek Wrona <wrona@syncad.com>
2023-11-19 15:45:30 +0100

    run_tests.sh script adjusted to new project structure

--- python/wax/tests/run_tests.sh
+++ python/wax/tests/run_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -e
+
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-poetry run pytest --junitxml="${SCRIPTPATH}/report.xml" "${SCRIPTPATH}"
+
+poetry -C "${SCRIPTPATH}/../../" run pytest --junitxml="${SCRIPTPATH}/report.xml" "${SCRIPTPATH}"
