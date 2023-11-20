ae524173065fef682df26d1a329a853cb78b3d4a
Bartek Wrona <wrona@syncad.com>
2023-11-20 15:44:19 +0100

    run_tests adjusted to new test directory location

--- python/tests/run_tests.sh
+++ python/tests/run_tests.sh
@@ -3,4 +3,4 @@ set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-poetry -C "${SCRIPTPATH}/../../" run pytest --junitxml="${SCRIPTPATH}/report.xml" "${SCRIPTPATH}"
+poetry -C "${SCRIPTPATH}/../" run pytest --junitxml="${SCRIPTPATH}/report.xml" "${SCRIPTPATH}"
