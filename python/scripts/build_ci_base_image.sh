358be27223da51e68a6355bd8f80addb828ddb1f
Bartek Wrona <wrona@syncad.com>
2023-11-21 17:05:57 +0100

    Base CI image changed to hive/ci-base-image:ubuntu23.10

--- python/scripts/build_ci_base_image.sh
+++ python/scripts/build_ci_base_image.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PROJECT_DIR="${SCRIPT_DIR}/../../"
 
 REGISTRY=${1:-registry.gitlab.syncad.com/hive/wax/}
-CI_IMAGE_TAG=:ubuntu22.04-1
-BASE_IMAGE=registry.gitlab.syncad.com/hive/hive/ci-base-image:ubuntu22.04-8
+CI_IMAGE_TAG=:ubuntu23.10-1
+BASE_IMAGE=registry.gitlab.syncad.com/hive/hive/ci-base-image:ubuntu23.10-2
 
 export DOCKER_BUILDKIT=1
 
