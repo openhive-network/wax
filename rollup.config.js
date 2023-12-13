1db00fa781f02f6d6ce6cadbdfe32e0c94d0f877
mtyszczak <mateusz.tyszczak@gmail.com>
2023-12-13 10:27:00 +0100

    Fix bundling wax for standalone browser script

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 
 export default {
   input: 'wasm/dist/index.js',
@@ -10,6 +11,10 @@ export default {
     name: 'wax'
   },
   plugins: [
+    replace({
+      'process': null,
+      'process.env': null
+    }),
     nodeResolve({ preferBuiltins: false, browser: true }),
     typescript({
       rollupCommonJSResolveHack: false,
