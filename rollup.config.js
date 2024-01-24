5a45650eaf4b59d1580cdad4c5555a87b2c92757
mtyszczak <mateusz.tyszczak@gmail.com>
2024-01-24 11:01:14 +0100

    Reduce bundle size for Node.js and web environments

--- rollup.config.js
+++ rollup.config.js
@@ -8,19 +8,19 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const commonConfiguration = env => ([
+const commonConfiguration = (env, packEntire = false) => ([
   {
     input: `wasm/dist/lib/${env}.js`,
     output: {
       format: 'es',
       name: 'wax',
-      file: `wasm/dist/bundle/${env}.js`
+      file: `wasm/dist/bundle/${env}${packEntire ? '-full' : ''}.js`
     },
     plugins: [
       alias({
         entries: [
           { find: '@hive/beekeeper', replacement: `@hive/beekeeper/${env}` },
-          { find: './build_wasm/wax.web.js', replacement: path.resolve(__dirname, `./wasm/lib/build_wasm/wax.${env}.js`) }
+          { find: './build_wasm/wax.web.js', replacement: `build_wasm/wax.${env}.js` }
         ]
       }),
       replace({
@@ -28,13 +28,23 @@ const commonConfiguration = env => ([
         'process.env': null,
         preventAssignment: true
       }),
-      nodeResolve({ preferBuiltins: env !== "web", browser: env === "web" }),
+      nodeResolve({
+        preferBuiltins: env !== "web",
+        browser: env === "web",
+        modulePaths: [
+          'wasm/dist/lib'
+        ],
+        resolveOnly: packEntire ? [] : [
+          'build_wasm',
+          '@hive/beekeeper'
+        ]
+      }),
       commonjs()
     ]
   }, {
     input: `wasm/dist/lib/${env}.d.ts`,
     output: [
-      { file: `wasm/dist/bundle/${env}.d.ts`, format: "es" }
+      { file: `wasm/dist/bundle/${env}${packEntire ? '-full' : ''}.d.ts`, format: "es" }
     ],
     plugins: [
       dts()
@@ -44,5 +54,6 @@ const commonConfiguration = env => ([
 
 export default [
   ...commonConfiguration('node'),
-  ...commonConfiguration('web')
+  ...commonConfiguration('web'),
+  ...commonConfiguration('web', true)
 ];
