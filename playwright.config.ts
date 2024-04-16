43b2f7612ed6b99a60d22f41c68c77779f6ea126
Bartek Wrona <wrona@syncad.com>
2024-04-16 22:10:57 +0200

    Defined separate PlayWright test suite specific to passed custom HiveChain options.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,8 @@
 // This is a workaround for https://github.com/microsoft/playwright/issues/18282#issuecomment-1612266345
 import { defineConfig } from '@playwright/test';
+import { IWaxedTest } from './wasm/__tests__/assets/jest-helper';
 
-export default defineConfig({
+export default defineConfig<IWaxedTest>({
   reporter: [
     ['junit', { outputFile: 'results.xml' }],
     ['json',  { outputFile: 'results.json' }]
@@ -10,7 +11,18 @@ export default defineConfig({
     {
       name: "wax_testsuite",
       testDir: "./wasm/dist"
+    },
+    {
+      name: "wax_testsuite_custom_chain_options",
+      testDir: "./wasm/dist",
+      testMatch: "hive_chain_custom_opts*",
+      use:
+      {
+        apiEndpointUrl: "http://use.me.now",
+        chainId: "42"
+      }
     }
+
   ],
   // Run your local dev server before starting the tests
   webServer: {
