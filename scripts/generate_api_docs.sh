442a75ce818c2ce974a76bfcaf440aa5aef78780
Bartek Wrona <wrona@syncad.com>
2024-05-23 19:51:50 +0000

    Enabled sourceLinkTemplate and version generation

--- scripts/generate_api_docs.sh
+++ scripts/generate_api_docs.sh
@@ -4,6 +4,9 @@ set -e
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PROJECT_DIR="${SCRIPTPATH}/.."
 
+REPO_URL="${1:?Missing repository url}"
+REVISION_INFO="${2:?Missing argument pointing git revision}"
+
 OUTPUT_DIR=wasm/dist/docs
 INPUT_FILE=wasm/lib/web.ts
 
@@ -13,6 +16,14 @@ pushd "${PROJECT_DIR}"
 
 mkdir -vp "${OUTPUT_DIR}"
 
-pnpm exec typedoc --readme npm.ts.md --plugin typedoc-plugin-markdown --plugin typedoc-gitlab-wiki-theme --tsconfig tsconfig.json --out "${OUTPUT_DIR}" "${INPUT_FILE}"
+pnpm exec typedoc --includeVersion \
+  --sourceLinkTemplate "${REPO_URL}/-/blob/{gitRevision}/{path}#L{line}" \
+  --gitRevision "${REVISION_INFO}" \
+  --readme npm.ts.md \
+  --plugin typedoc-plugin-markdown \
+  --plugin typedoc-gitlab-wiki-theme \
+  --tsconfig tsconfig.json \
+  --out "${OUTPUT_DIR}" \
+  "${INPUT_FILE}"
 
 popd
