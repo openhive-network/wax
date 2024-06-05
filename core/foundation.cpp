61d1afe08bd6fc97651bc566ddd41984505ad3fb
Bartek Wrona <wrona@syncad.com>
2024-06-05 23:29:35 +0200

    cpp_get_public_key_from_signature should return prefixed public keys

--- core/foundation.cpp
+++ core/foundation.cpp
@@ -260,7 +260,7 @@ result foundation::cpp_get_public_key_from_signature(const std::string& digest, 
 
     fc::from_hex(signature, reinterpret_cast<char*>(&*sig.begin()), sig.size());
 
-    _result.content = fc::ecc::public_key::to_base58(fc::ecc::public_key{ sig, d }, false);
+    _result.content = fc::ecc::public_key::to_base58_with_prefix(fc::ecc::public_key{ sig, d }, HIVE_ADDRESS_PREFIX);
   });
 }
 
