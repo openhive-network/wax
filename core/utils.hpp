52892dc321910d250edd7dabb46f4f2d1b8686e0
Bartek Wrona <wrona@syncad.com>
2024-06-17 23:23:58 +0200

    Implemented safe_exception_wrapper helper function

--- core/utils.hpp
+++ core/utils.hpp
@@ -2,6 +2,7 @@
 
 #include <exception>
 #include <functional>
+#include <stdexcept>
 
 #include <fc/exception/exception.hpp>
 
@@ -35,6 +36,54 @@ static result method_wrapper(callback&& method)
   }
 
   return _result;
+}
+
+/** Allows to wrap given function call into exception handler which at most throw std::exception (safely handled by Python/WASM runtimes)
+*/
+template <typename ProcessorFn, typename... Args>
+static decltype(auto) safe_exception_wrapper(ProcessorFn fn, Args&&... args)
+{
+  try
+  {
+    return fn(std::forward<Args>(args)...);
+  }
+  catch (fc::exception& e)
+  {
+    wlog("Caught fc::exception: ${details}", ("details", e.to_detail_string()));
+    throw std::runtime_error(e.to_detail_string());
+  }
+  catch (const std::exception& e)
+  {
+    wlog("Caught std::exception: ${details}", ("details", e.what()));
+    throw;
+  }
+  catch (...)
+  {
+    std::exception_ptr e = std::current_exception();
+    if(e)
+    {
+      try
+      {
+        /// give it last chance to be recognized
+        std::rethrow_exception(e);
+      }
+      catch (const std::exception& ex)
+      {
+        wlog("Caught std::exception: ${details}", ("details", ex.what()));
+        throw;
+      }
+      catch (...)
+      {
+        wlog("Caught nonstandard exception");
+        throw std::runtime_error("Nonstanard exception");
+      }
+    }
+    else
+    {
+      wlog("Caught unknown exception");
+      throw std::runtime_error("Unknown exception.");
+    }
+  }
 }
 
 } /// namespace cpp
