fbe02d1a372a0d065741143eecd1b63b3045e699
Bartek Wrona <wrona@syncad.com>
2024-07-08 22:05:04 +0200

    Implemented python wrappers: serialize_witness_set_properties and deserialize_witness_set_properties

--- python/wax.pyx
+++ python/wax.pyx
@@ -1,11 +1,16 @@
 # distutils: language = c++
+
 from functools import wraps
 
+from libcpp.string cimport string as cppstring
 from libcpp.set cimport set as cppset
+from libcpp.optional cimport optional, make_optional
+from libc.stdint cimport uint16_t, uint32_t, int32_t
+
 from cython.operator cimport dereference, preincrement
 
 from wax cimport error_code, json_asset, result, protocol, proto_protocol
-from .wax_result import python_result, python_error_code, python_json_asset, python_ref_block_data, python_required_authority_collection, python_encrypted_memo, python_private_key_data, python_brain_key_data
+from .wax_result import python_result, python_error_code, python_json_asset, python_ref_block_data, python_required_authority_collection, python_encrypted_memo, python_private_key_data, python_brain_key_data, python_witness_set_properties_data, python_price
 
 def return_python_result(foo):
     @wraps(foo)
@@ -291,7 +296,7 @@ def encode_encrypted_memo(encrypted_content: string, main_encryption_key: string
     cdef protocol obj
     cdef crypto_memo data_to_encode
     data_to_encode._from = main_encryption_key
-    if other_encryption_key == '':
+    if other_encryption_key == b'':
       other_encryption_key = main_encryption_key
 
     data_to_encode.to = other_encryption_key
@@ -307,6 +312,140 @@ def decode_encrypted_memo(encoded_memo: string) -> python_encrypted_memo:
       other_encryption_key = decoded.to,
       encrypted_content = decoded.content
     )
+
+def serialize_witness_set_properties(input_props: python_witness_set_properties_data) -> dict[string, string]:
+    cdef protocol obj
+    cdef witness_set_properties_data _props_to_serialize
+    _props_to_serialize.key = input_props.key
+    cdef optional[string] str_opt
+    cdef cppstring c_str
+
+    cdef uint16_t _rate_helper
+    cdef optional[uint16_t] _rate_opt
+
+    cdef int32_t _subsidy_budget
+    cdef optional[int32_t] subsidy_budget
+    cdef uint32_t _uint_helper
+    cdef optional[uint32_t] _uint_opt
+
+    cdef json_asset _base
+    cdef json_asset _quote
+    cdef price _price_helper
+    cdef optional[price] _price_opt
+
+    if input_props.new_signing_key is not None:
+      if isinstance(input_props.new_signing_key, str):
+        byte_string = input_props.new_signing_key.encode('utf-8')
+      else:
+        byte_string = input_props.new_signing_key
+
+      c_str = byte_string
+      str_opt=c_str
+      _props_to_serialize.new_signing_key=str_opt
+
+    if input_props.account_creation_fee is not None:
+      _base = json_asset(input_props.account_creation_fee.amount,
+        input_props.account_creation_fee.precision,
+        input_props.account_creation_fee.nai
+        )
+      _props_to_serialize.account_creation_fee=_base
+
+    if input_props.url is not None:
+      if isinstance(input_props.url, str):
+        byte_string = input_props.url.encode('utf-8')
+      else:
+        byte_string = input_props.url
+
+      c_str = byte_string
+      str_opt=c_str
+      _props_to_serialize.url=str_opt
+
+    if input_props.hbd_exchange_rate is not None:
+      _base = json_asset(input_props.hbd_exchange_rate.base.amount,
+        input_props.hbd_exchange_rate.base.precision,
+        input_props.hbd_exchange_rate.base.nai)
+
+      _quote = json_asset(input_props.hbd_exchange_rate.quote.amount,
+        input_props.hbd_exchange_rate.quote.precision,
+        input_props.hbd_exchange_rate.quote.nai)
+
+      _price_helper.base=_base
+      _price_helper.quote=_quote
+
+      _price_opt = _price_helper
+      _props_to_serialize.hbd_exchange_rate=_price_opt 
+
+    if input_props.maximum_block_size is not None:
+      _uint_helper=int(input_props.maximum_block_size)
+      _uint_opt=_uint_helper
+      _props_to_serialize.maximum_block_size=_uint_opt
+
+    if input_props.hbd_interest_rate is not None:
+      _rate_helper=input_props.hbd_interest_rate
+      _rate_opt=_rate_helper
+      _props_to_serialize.hbd_interest_rate=_rate_opt
+
+    if input_props.account_subsidy_budget is not None:
+      _subsidy_budget=int(input_props.account_subsidy_budget)
+      subsidy_budget = _subsidy_budget
+      _props_to_serialize.account_subsidy_budget=subsidy_budget
+
+    if input_props.account_subsidy_decay is not None:
+      _uint_helper=int(input_props.account_subsidy_decay)
+      _uint_opt=_uint_helper
+      _props_to_serialize.account_subsidy_decay=_uint_opt
+
+    serialized_properties = obj.cpp_serialize_witness_set_properties(_props_to_serialize)
+    return serialized_properties
+
+def deserialize_witness_set_properties(serialized_properties: dict[string, string]) -> python_witness_set_properties_data:
+    cdef protocol obj
+    cdef witness_set_properties_serialized _serialized_props
+
+    for prop, value in serialized_properties.items():
+      _serialized_props[prop] = value
+
+    deserialized_props = obj.cpp_deserialize_witness_set_properties(_serialized_props)
+
+    ret_val=python_witness_set_properties_data(key=deserialized_props.key)
+
+    if deserialized_props.new_signing_key.has_value():
+      ret_val.new_signing_key = bytes(deserialized_props.new_signing_key.value())
+
+    if deserialized_props.account_creation_fee.has_value():
+      ret_val.account_creation_fee = python_json_asset(deserialized_props.account_creation_fee.value().amount,
+        deserialized_props.account_creation_fee.value().precision,
+        deserialized_props.account_creation_fee.value().nai
+        )
+
+    if deserialized_props.url.has_value():
+      ret_val.url = bytes(deserialized_props.url.value())
+
+    if deserialized_props.hbd_exchange_rate.has_value():
+      _source=deserialized_props.hbd_exchange_rate.value()
+      _base = python_json_asset(_source.base.amount,
+        _source.base.precision,
+        _source.base.nai)
+
+      _quote = python_json_asset(_source.quote.amount,
+        _source.quote.precision,
+        _source.quote.nai)
+
+      ret_val.hbd_exchange_rate=python_price(base=_base, quote=_quote)
+
+    if deserialized_props.maximum_block_size.has_value():
+      ret_val.maximum_block_size=int(deserialized_props.maximum_block_size.value())
+
+    if deserialized_props.hbd_interest_rate.has_value():
+      ret_val.hbd_interest_rate=int(deserialized_props.hbd_interest_rate.value())
+
+    if deserialized_props.account_subsidy_budget.has_value():
+      ret_val.account_subsidy_budget=int(deserialized_props.account_subsidy_budget.value())
+
+    if deserialized_props.account_subsidy_decay.has_value():
+      ret_val.account_subsidy_decay=int(deserialized_props.account_subsidy_decay.value())
+
+    return ret_val
 
 def verify_exception_handling(throw_type: int) -> None:
     cdef protocol obj
