f5a335a1b6072d2a01ec94b1edddac9ceead776f
mtyszczak <mateusz.tyszczak@gmail.com>
2024-07-08 12:14:54 +0200

    Remove redundant WASM code messing with Next app bundling

--- rollup.config.js
+++ rollup.config.js
@@ -22,11 +22,25 @@ const commonConfiguration = packEntire => ([
       replace({
         delimiters: ['', ''],
         values: {
+          // Generated Emscripten WASM code contains fs, which is not actually used by our code, so remove it to prevent client bundler errors:
+          'fs.readFileSync(filename,binary?undefined:"utf8")': null,
+          'fs.readFile(filename,binary?undefined:"utf8",(err,data)=>{if(err)onerror(err);else onload(binary?data.buffer:data)})': null,
+          'fs.readSync(fd,buf)': '0', // fallback - readSync returns the number of bytesRead
+          // Instead of fs we need crypto module in Node.js environment later for SSL initRandomDevice code - ensure proper module is imported:
+          'var fs=require("fs")': 'var node_crypto=await import("crypto")',
+          'require("crypto")': 'node_crypto',
+          // 'module' dependency is redundant in our environment - use 'await import' instead:
+          'const{createRequire:createRequire}=await import("module");': '',
+          'var require=createRequire(import.meta.url);': '',
+          'var nodePath=require("path")': 'var nodePath=await import("path")',
+          // new URL("./") throws - use import.meta.url instead:
           'require("url").fileURLToPath(new URL("./",import.meta.url))': 'import.meta.url',
+          // Hardcode package name and version for later use in the code:
           'process.env.npm_package_name': `"${process.env.npm_package_name}"`,
           'process.env.npm_package_version': `"${process.env.npm_package_version}"`,
           // WASM requires process.argv[1] argument to be set. We can mock it in web browser environment:
           'process.argv': '(typeof process=="object"&&typeof process.argv=="object"?process.argv:["",""])',
+          // Keeping process.env in the code will cause errors in the browser environment - remove it:
           'process.env': null
         },
         preventAssignment: true
