d1c67a6d0ee9fef99dc6dc551814630757665fd0
mtyszczak <mateusz.tyszczak@gmail.com>
2024-07-08 13:08:09 +0200

    Use server for parcel examples

--- examples/ts/index.ts
+++ examples/ts/index.ts
@@ -2,6 +2,7 @@ import { ConsoleMessage, chromium } from 'playwright';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { Parcel } from '@parcel/core';
+import { createServer } from 'http-server';
 
 declare type ExamplesWindow = typeof window & typeof globalThis & { exampleFinished?: true };
 
@@ -18,11 +19,16 @@ const configFor = (name: string): ConstructorParameters<typeof Parcel>[0] => ({
 });
 
 (async () => {
+  const port = 8000;
+
+  const server = createServer({
+    root: path.resolve(__dirname, 'dist')
+  });
+
+  server.listen(port);
+
   const browser = await chromium.launch({
-    headless: true,
-    args: [
-      '--disable-web-security', // Disable CORS - parcel generates helper functions in separate files that are loaded via file://
-    ],
+    headless: true
   });
   const page = await browser.newPage();
   page.on('console', (msg: ConsoleMessage) => {
@@ -34,7 +40,7 @@ const configFor = (name: string): ConstructorParameters<typeof Parcel>[0] => ({
     const bundle = new Parcel(configFor(what));
     await bundle.run();
     console.debug('Bundled');
-    await page.goto(`file://${__dirname}/dist/${what}.html`);
+    await page.goto(`http://localhost:${port}/${what}.html`);
     await page.waitForURL(`**/${what}.html`, { waitUntil: 'load' });
     // Set maximum examples execution time to 3 seconds. Fail after that time
     await page.waitForFunction(() => (window as ExamplesWindow).exampleFinished === true, undefined, { timeout: 3000 });
@@ -46,6 +52,7 @@ const configFor = (name: string): ConstructorParameters<typeof Parcel>[0] => ({
   await open('proto_sign_transaction.example');
 
   await browser.close();
+  server.close();
 
   console.debug('Done running examples');
 })().catch((error: unknown) => {
