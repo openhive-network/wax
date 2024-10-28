73a25e633f7ddb8f120d5b0b8a5d7444bdbf9332
Bartek Wrona <wrona@syncad.com>
2024-10-28 23:23:19 +0100

    calculateExpiration function allows to pass explicit base time for relative expiration times.

--- ts/wasm/lib/detailed/util/expiration_parser.ts
+++ ts/wasm/lib/detailed/util/expiration_parser.ts
@@ -1,17 +1,21 @@
 import { WaxError } from "../../errors.js";
 
-export const calculateExpiration = (expirationTime?: number | string | Date): Date | void => {
+export const dateFromString = (dateTimeString: string): Date => {
+  if (dateTimeString.endsWith('Z'))
+    return new Date(dateTimeString);
+  else
+    return new Date(`${dateTimeString}Z`);
+};
+
+export const calculateExpiration = (referenceTime?: Date, expirationTime?: number | string | Date): Date | void => {
   // Transaction directly initialized from protobuf JSON or API. No expiration time given, so do not apply expiration time
   if(typeof expirationTime === 'undefined')
     return;
 
   let expiration: Date;
   if(typeof expirationTime === 'string') {
-    if(expirationTime[0] !== "+")
-      if(expirationTime.endsWith('Z'))
-        return new Date(expirationTime);
-      else
-        return new Date(`${expirationTime}Z`);
+    if (expirationTime[0] !== "+")
+      return dateFromString(expirationTime);
 
     let mul = 1000;
 
@@ -27,7 +31,10 @@ export const calculateExpiration = (expirationTime?: number | string | Date): Da
     if(Number.isNaN(num))
       throw new WaxError("Invalid expiration time offset");
 
-    expiration = new Date(Date.now() + (num * mul));
+    if (referenceTime === undefined)
+      referenceTime = new Date(Date.now());
+
+    expiration = new Date(referenceTime.getTime() + (num * mul));
   } else {
     expiration = new Date(expirationTime);
   }
