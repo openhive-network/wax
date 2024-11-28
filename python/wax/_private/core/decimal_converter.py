aa1673ca311cd593fe6c85d5d2347dcb3789b350
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-11-28 10:04:18 +0100

    Include the core package in the private part

---
+++ python/wax/_private/core/decimal_converter.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from decimal import Decimal, InvalidOperation
+
+from schemas.fields.hive_int import HiveInt
+from wax._private.exceptions import DecimalConversionNegativePrecisionError, DecimalConversionNotANumberError
+
+DecimalConvertible = int | float | Decimal | HiveInt
+
+
+class DecimalConverter:
+    @classmethod
+    def get_precision(cls, amount: DecimalConvertible) -> int:
+        """
+        Get precision of given amount.
+
+        Args:
+            amount: Amount to get precision of.
+
+        Raises:
+            DecimalConversionNotANumberError: Raised when given amount is in invalid format.
+        """
+        converted = cls.convert(amount)
+        exponent = int(converted.as_tuple().exponent)
+        return -1 * exponent
+
+    @classmethod
+    def convert(cls, amount: DecimalConvertible, *, precision: int | None = None) -> Decimal:
+        """
+        Convert given amount to Decimal.
+
+        Args:
+            amount: Amount to convert.
+            precision: Precision of the amount.
+
+        Returns:
+            Decimal: amount converted into the decimal.
+
+        Raises:
+            DecimalConversionNotANumberError: Raised when given amount is in invalid format.
+            DecimalConversionNegativePrecisionError: If given precision is negative.
+        """
+        try:
+            converted = Decimal(amount)
+        except InvalidOperation as error:
+            raise DecimalConversionNotANumberError(f"Given {amount=} is not a number.") from error
+
+        if precision is not None:
+            cls._assert_precision_is_positive(precision)
+            converted = cls.round_to_precision(converted, precision)
+
+        return converted
+
+    @staticmethod
+    def round_to_precision(amount: Decimal, precision: int) -> Decimal:
+        exponent = Decimal(10) ** (-1 * precision)
+        return amount.quantize(exponent)
+
+    @staticmethod
+    def _assert_precision_is_positive(precision: int) -> None:
+        if precision < 0:
+            raise DecimalConversionNegativePrecisionError(precision)
