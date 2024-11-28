103935d03f708b799725a8b483bc10306ab09cf9
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-11-28 10:04:18 +0100

    Include the core package in the private part

---
+++ python/wax/_private/core/constants.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from typing import Final
+
+PUBLIC_KEY_ADDRESS_PREFIX: Final[str] = "STM"
---
+++ python/wax/_private/core/python_price_converter.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from wax._private.models.asset import Asset, AssetName
+from wax.wax_result import python_price
+
+if TYPE_CHECKING:
+    from wax._private.models.asset import HbdNaiAssetConvertible, HiveNaiAssetConvertible
+
+
+def convert_to_python_price(base: HbdNaiAssetConvertible, quote: HiveNaiAssetConvertible) -> python_price:
+    asset_handler = Asset()
+
+    base = asset_handler.resolve_from_convertible_type(AssetName.Hbd, base)
+    quote = asset_handler.resolve_from_convertible_type(AssetName.Hive, quote)
+
+    return python_price(
+        base=asset_handler.to_python_json_asset(base),
+        quote=asset_handler.to_python_json_asset(quote),
+    )
