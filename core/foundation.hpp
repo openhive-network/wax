db0ac4b06048ebca962710f90c82409949032cf6
Bartek Wrona <wrona@syncad.com>
2024-12-10 18:04:12 +0100

    Eliminated #include config.hpp causing dangerous runtime problems

--- core/foundation.hpp
+++ core/foundation.hpp
@@ -2,7 +2,6 @@
 
 #include "core/types.hpp"
 #include <map>
-#include <hive/protocol/config.hpp>
 
 #include <string>
 
@@ -23,7 +22,7 @@ public:
   /** Returns map of hive::protocol constants in form:
   *   constant_name => constant_value as string.
   */
-  std::map<std::string, std::string> cpp_get_hive_protocol_config(const std::string& chain_id = HIVE_CHAIN_ID);
+  std::map<std::string, std::string> cpp_get_hive_protocol_config(const std::string& chain_id);
 
   ///  Allows to retrieve public key in WIF format from the signature and digest in hexadecimal format
   result cpp_get_public_key_from_signature(const std::string& digest, const std::string& signature);
