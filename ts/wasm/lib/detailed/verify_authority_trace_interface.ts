d49a18fb02d84e21bbbf39fef7269a0e55cf445c
Bartek Wrona <wrona@syncad.com>
2024-12-11 14:22:27 +0100

    Definition of authority trace data structures exposed to TS

---
+++ ts/wasm/lib/detailed/verify_authority_trace_interface.ts
@@ -0,0 +1,123 @@
+import type { TPublicKey } from "@hiveio/beekeeper";
+import type { TAccountName } from './hive_apps_operations';
+
+export type TAuthorityRole = string; /// TODO: unify with role type defined in account authority update meta operation
+
+/**
+ * Collects information during given authority processing when it was rejected.
+ *
+ * @category Authority
+ */
+export type TAuthorityEntryVerificationFailure = {
+  entryAccepted: false;
+  /**
+   * true if given authority processing has been interrupted by crossing recursion limit
+   */
+  accountAuthorityProcessingDepthExceeded: boolean;
+  /**
+   * true if given path entry processing has been interrupted by crossing total number of processed account redirections
+   */
+  accountAuthorityCountExceeded: boolean;
+  /**
+   * true if given path entry points to the account not known by the blockchain
+   */
+  accountAuthorityPointsMissingAccount: boolean;
+  /**
+   * true if given path entry created a cycle while processig authority account redirection
+   */
+  hasAccountAuthorityCycle: boolean;
+  /**
+   * true when key/account entry matched, but the weight was insufficient
+   */
+  hasInsufficientWeight: boolean;
+  /**
+   * - true when authority has matching any key, but the weight is insufficient
+   * - false when authority does not contain any matching key
+  */
+  hasMatchingPublicKey: boolean;
+};
+
+/**
+ * Determines set of information collected when given authority path entry has been processed successfully.
+ *
+ * @category Authority
+ */
+export type TAuthorityEntryVerificationSuccess = {
+  entryAccepted: true;
+  /**
+   * - true when authority is open
+   * - false when any key in the authority matched (it implies sufficient weight)
+   */
+  isOpenAuthority: boolean;
+};
+
+/**
+ * Determines set of information collected during processing given authority path entry.
+ *
+ * @category Authority
+ */
+export type TAuthorityEntryProcessingStatus = TAuthorityEntryVerificationSuccess | TAuthorityEntryVerificationFailure;
+
+/**
+ * Holds data specific to single authority path entry.
+ * Authority paths are created while processing Account authority definitions, which contain entries describing authority details (built over account or direct key).
+ * 
+ * @category Authority
+ */
+export interface IAuthorityPathEntry {
+  /**
+   * Account name or public key specified at given authority entry definition.
+   */
+  processedEntry: TAccountName | TPublicKey;
+  /**
+   * Role level (posting/active/owner) specific to processed authority entry.
+   */
+  processedRole: TAuthorityRole;
+  /**
+   * A threashold specific to processed authority role definition.
+   */
+  threshold: number;
+  /**
+   * A weight specific to processed (account or key) entry definition.
+   */
+  weight: number;
+
+  /**
+   * Current processing nest level (in case of account authority redirection it can be > 1).
+   */
+  recursionDepth: number;
+
+  /**
+   * Holds set of gathered information described by {@link TAuthorityEntryProcessingStatus} type specific to given entry.
+   */
+  processingStatus: TAuthorityEntryProcessingStatus;
+
+  /**
+   * Holds entries being visited during traversing redirected account authority definifions.
+   * This structure allows to see all paths entered during authority verification process, i.e. just to simplify debugging or analysis.
+   */
+  visitedEntries: Array<IAuthorityPathEntry>;
+}
+
+/**
+ * Holds data produced during authority verification process.
+ * 
+ * @category Authority
+ */
+export interface IVerifyAuthorityTrace
+ {
+  /**
+   * Holds informations specific to the account which signed a transaction.
+   */
+  rootEntry: IAuthorityPathEntry;
+  /**
+   * Stores data specific to the authority path chosen:
+   * - if verification process has been satisfied, it contains successfull path
+   * - when it failed points last processed path.
+   */
+  finalAuthorityPath: Array<IAuthorityPathEntry>;
+  /**
+   * Holds set of gathered information described by {@link TAuthorityEntryProcessingStatus} type specific to whole authority verification process.
+   */
+  verificationStatus: TAuthorityEntryProcessingStatus;
+};
