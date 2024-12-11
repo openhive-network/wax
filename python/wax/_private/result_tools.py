e1b98ad0131c45ad3f43f01803a8e1ed4a7a62ee
Jakub Ziebinski <ziebinskijakub@gmail.com>
2024-12-11 15:58:58 +0100

    Implementation of the decode impacted account names function

--- python/wax/_private/result_tools.py
+++ python/wax/_private/result_tools.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 from typing import Any
 
+from pydantic import ValidationError
+
+from schemas.fields.basic import AccountName
 from wax._private.exceptions import WaxValidationFailedError
 from wax.wax_result import python_error_code, python_result
 
@@ -17,3 +20,22 @@ def validate_wax_result(result: python_result) -> None:
 
 def expose_result(result: python_result) -> Any:  # noqa: ANN401
     return result.result.decode()
+
+
+def decode_impacted_account_names(account_names: list[bytes]) -> list[AccountName]:
+    """
+    Decode account names from bytes to AccountName models.
+
+    Args:
+        account_names: List of account names in bytes.
+
+    Returns:
+        List of validated account names.
+
+    Raises:
+        WaxValidationFailedError: If account names are not valid.
+    """
+    try:
+        return [AccountName(account_name.decode()) for account_name in account_names]
+    except ValidationError as error:
+        raise WaxValidationFailedError("Error while parsing impacted accounts.") from error
