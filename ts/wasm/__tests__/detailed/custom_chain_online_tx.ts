871ff1425bb5a0cc5c9082dc756489fb2a7a4652
Bartek Wrona <wrona@syncad.com>
2024-12-16 22:41:28 +0100

    Private key leak tests extended by case covering implicit verification performed by IHiveChainInterface.broadcast call.

--- ts/wasm/__tests__/detailed/custom_chain_online_tx.ts
+++ ts/wasm/__tests__/detailed/custom_chain_online_tx.ts
@@ -6,7 +6,7 @@ import fs from "fs";
 
 import { test } from '../assets/jest-helper';
 import { protoVoteOp } from "../assets/data.proto-protocol";
-import { WaxPrivateKeyLeakDetectedException } from '../../dist/bundle/index-full';
+import { IWaxOptionsChain, WaxPrivateKeyLeakDetectedException } from '../../dist/bundle/index-full';
 
 import { IOnlineTransaction, operation, transfer } from '../../dist/bundle/index-full';
 
@@ -29,60 +29,62 @@ test.describe('Wax chain tests to cover Online Transaction flow', () => {
     await page.goto("http://localhost:8080/wasm/__tests__/assets/test.html", { waitUntil: "load" });
   });
 
+  const txSecurityLeakBody = async ({ beekeeper, wax }, mirrornetSkeletonKey: string, config: IWaxOptionsChain, directBroadcast: boolean) => {
+    // Create wallet:
+    const session = beekeeper.createSession("salt");
+    const { wallet } = await session.createWallet("w0");
+    const matchingPublicKey = await wallet.importKey(mirrornetSkeletonKey);
+
+    const myCustomChain = await wax.createHiveChain(config);
+
+    // Create online transaction
+    const tx: IOnlineTransaction = await myCustomChain.createTransaction();
+
+    // Fill it with some operation
+    const transferOp: transfer = {
+      from_account: "otom",
+      to_account: "otom",
+      amount: myCustomChain.hiveCoins(1),
+      memo: `${mirrornetSkeletonKey}`
+    };
+
+    const op: operation = { transfer: transferOp };
+    tx.pushOperation(op);
+
+    try {
+      if(directBroadcast)
+        await myCustomChain.broadcast(tx);
+      else
+        await tx.performOnChainVerification();
+    }
+    catch(e) {
+      const error: object = e as object;
+
+      if (e instanceof wax.WaxPrivateKeyLeakDetectedException) {
+        const caughtError: WaxPrivateKeyLeakDetectedException = error as WaxPrivateKeyLeakDetectedException;
+
+        return {
+          detectedLeakError: {
+            account: caughtError.account,
+            authorityRole: caughtError.authorityRole,
+            matchingPublicKey: caughtError.matchingPublicKey,
+            message: caughtError.message
+          }
+        };
+      } else
+        throw new Error("Invalid error instance");
+    }
+
+    tx.sign(wallet, matchingPublicKey);
+
+    throw new Error("No error detected");
+  };
+
   const mirrornetSkeletonKey = '5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n';
   const mirrornetSkeletonPublicKey = 'STM6LLegbAgLAy28EHrffBVuANFWcFgmqRMW13wBmTExqFE9SCkg4';
 
-  test('Should catch private key leak using online transaction interface', async ({ waxTest, config }) => {
-      const retVal = await waxTest(async({ beekeeper, wax }, mirrornetSkeletonKey) => {
-        // Create wallet:
-        const session = beekeeper.createSession("salt");
-        const { wallet } = await session.createWallet("w0");
-        const matchingPublicKey = await wallet.importKey(mirrornetSkeletonKey);
-
-        console.log(`${matchingPublicKey}`);
-
-        const myCustomChain = await wax.createHiveChain(config);
-
-        // Create online transaction
-        const tx: IOnlineTransaction = await myCustomChain.createTransaction();
-
-        // Fill it with some operation
-        const transferOp: transfer = {
-          from_account: "otom",
-          to_account: "otom",
-          amount: myCustomChain.hiveCoins(1),
-          memo: `${mirrornetSkeletonKey}`
-        };
-
-        const op: operation = { transfer: transferOp };
-        tx.pushOperation(op);
-
-        try {
-          await tx.performOnChainVerification();
-        }
-        catch(e) {
-          const error: object = e as object;
-
-          if (e instanceof wax.WaxPrivateKeyLeakDetectedException) {
-            const caughtError: WaxPrivateKeyLeakDetectedException = error as WaxPrivateKeyLeakDetectedException;
-
-            return {
-              detectedLeakError: {
-                account: caughtError.account,
-                authorityRole: caughtError.authorityRole,
-                matchingPublicKey: caughtError.matchingPublicKey,
-                message: caughtError.message
-              }
-            };
-          } else
-            throw new Error("Invalid error instance");
-        }
-
-        tx.sign(wallet, matchingPublicKey);
-
-        throw new Error("No error detected");
-
-      }, mirrornetSkeletonKey);
+  test('Should catch private key leak using online transaction interface during explicit online validation', async ({ waxTest, config }) => {
+      const retVal = await waxTest(txSecurityLeakBody, mirrornetSkeletonKey, config!, false);
     expect(retVal.detectedLeakError).toStrictEqual({
       account: "otom",
       authorityRole: "owner",
@@ -90,6 +92,16 @@ test.describe('Wax chain tests to cover Online Transaction flow', () => {
       message: "Detected private key leak."
     });
   });
+
+  test('Should catch private key leak using online transaction interface during direct broadcast', async ({ waxTest, config }) => {
+    const retVal = await waxTest(txSecurityLeakBody, mirrornetSkeletonKey, config!, true);
+  expect(retVal.detectedLeakError).toStrictEqual({
+    account: "otom",
+    authorityRole: "owner",
+    matchingPublicKey: mirrornetSkeletonPublicKey,
+    message: "Detected private key leak."
+  });
+});
 
   test('Should be able to create and sign transaction using online transaction interface', async ({ waxTest, config }) => {
     const retVal = await waxTest(async({ beekeeper, wax }, protoVoteOp, mirrornetSkeletonKey, mirrornetSkeletonPublicKey) => {
