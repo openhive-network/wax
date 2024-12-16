24cf0e1effe6557a43369119eaac055fea8da638
Bartek Wrona <wrona@syncad.com>
2024-12-16 09:44:56 +0100

    Implemented waxStdException processing scheme

--- ts/wasm/lib/detailed/util/wasm_errors.ts
+++ ts/wasm/lib/detailed/util/wasm_errors.ts
@@ -1,17 +1,63 @@
 import { WaxError } from "../../errors.js";
 
-export const safeWasmCall = <T extends () => any>(fn: T): ReturnType<T> => {
+export type TWaxStdExceptionData = {
+  msg: string;
+  data: object;
+  sourceException: Error|WebAssembly.RuntimeError
+};
+
+export type TCustomExceptionHandlerFunction = (error: TWaxStdExceptionData) => void;
+
+const handleWaxStdException = (e: any, customExceptionHandler?: TCustomExceptionHandlerFunction): void => {
+  /// unfortunately we can't use instanceof FinalExceptionClass here (probably because of Playwright's context isolation)
+  if(e instanceof Object && "message" in e) {
+    const eObject: {message: string} = e as {message: string};
+    /// Warning: toString() is necessary here, because otherwise string methods do not work
+    const msg = eObject.message.toString();
+
+    if(customExceptionHandler !== undefined && msg.indexOf("WAX_STD_EXCEPTION") > 0) {
+      const jsonBody = msg.replace("std::runtime_error,", "");
+      const contextMsg = JSON.parse(jsonBody);
+      //console.log(`Received contextMsg: ${JSON.stringify(contextMsg)}`);
+
+      const waxStdExceptionData: TWaxStdExceptionData = {
+        msg: contextMsg.msg,
+        data: contextMsg,
+        sourceException: e as Error|WebAssembly.RuntimeError
+      };
+
+      customExceptionHandler(waxStdExceptionData);
+
+    throw new WaxError(`Error during Wasm call: ${msg}`);
+    }
+  }
+
+  //console.log("Non-typed Error during Wasm call...", e);
+  //throw new WaxError(`Non-typed Error during Wasm call: ${e}`);
+};
+
+/**
+ * Allows for safe call to wasm function that may throw an error.
+ * @param fn WASM function to be called
+ * @param customExceptionHandler optional custom exception handler which can try to recognize thrown Error and handle it in more specific way
+ * If customExceptionHandler does not throw, standard WaxError will be thrown.
+  */
+export const safeWasmCall = <T extends () => any>(fn: T, customExceptionHandler?: TCustomExceptionHandlerFunction): ReturnType<T> => {
   try {
     return fn()
-  } catch (error) {
-    throw new WaxError(`Error during Wasm call: ${error instanceof Error ? error.message : error}`);
+  } catch (e) {
+    handleWaxStdException(e, customExceptionHandler);
+    //console.log("Non-typed Error during Wasm call...", e);
+    throw new WaxError(`Non-typed Error during Wasm call: ${e}`); // it should be inside handleWaxStdException but compiler complains about missing retval
   }
 };
 
-export const safeAsyncWasmCall = async <T extends () => any>(fn: T): Promise<ReturnType<T>> => {
+export const safeAsyncWasmCall = async <T extends () => any>(fn: T, customExceptionHandler?: TCustomExceptionHandlerFunction): Promise<ReturnType<T>> => {
   try {
     return await fn();
-  } catch (error) {
-    throw new WaxError(`Error during Wasm call: ${error instanceof Error ? error.message : error}`);
+  } catch (e) {
+    handleWaxStdException(e, customExceptionHandler);
+    //console.log("Non-typed Error during Wasm call...", e); 
+    throw new WaxError(`Non-typed Error during Wasm call: ${e}`); // it should be inside handleWaxStdException but compiler complains about missing retval
   }
 };
