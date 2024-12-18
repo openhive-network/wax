7b7972e83027ef4ccada7e70baf950ff5e0f062c
kmochocki <kmochocki@syncad.com>
2024-12-18 13:05:53 +0000

    Apply bytes/str conversion functions

--- python/wax/_private/models/operations.py
+++ python/wax/_private/models/operations.py
@@ -8,6 +8,7 @@ from google.protobuf.json_format import MessageToJson
 from schemas.operation import Operation
 from schemas.operations.representation_types import Hf26OperationRepresentationType
 from schemas.operations.representations import HF26Representation, convert_to_representation
+from wax._private.core.encoders import to_cpp_string, to_python_string
 from wax._private.exceptions import InvalidOperationFormatError, WaxValidationFailedError
 from wax._private.result_tools import validate_wax_result
 from wax.cpp_python_bridge import api_to_proto, proto_to_api
@@ -22,40 +23,40 @@ OperationHF26: TypeAlias = (
 
 def prepare_operation_to_validate(operation: OperationHF26) -> bytes:
     if isinstance(operation, operation_pb2.operation):
-        operation = proto_to_api(MessageToJson(operation).encode())
+        operation = proto_to_api(to_cpp_string(MessageToJson(operation)))
 
     if isinstance(operation, bytes):
         return operation
 
     if isinstance(operation, str):
-        return operation.encode()
+        return to_cpp_string(operation)
 
     if isinstance(operation, python_result):
         validate_wax_result(operation)
         return operation.result
 
     if isinstance(operation, HF26Representation):
-        return operation.json(by_alias=True).encode()
+        return to_cpp_string(operation.json(by_alias=True))
 
     if isinstance(operation, Operation):
-        return cast(HF26Representation, convert_to_representation(operation)).json(by_alias=True).encode()
+        return to_cpp_string(cast(HF26Representation, convert_to_representation(operation)).json(by_alias=True))
 
-    return json.dumps(operation).encode()
+    return to_cpp_string(json.dumps(operation))
 
 
 def prepare_operation_to_get_impacted_accounts(operation: OperationHF26) -> bytes:
     if isinstance(operation, bytes):
-        operation = operation.decode()
+        operation = to_python_string(operation)
 
     if isinstance(operation, str):
         operation = json.loads(operation)
 
     if isinstance(operation, python_result):
         validate_wax_result(operation)
-        operation = operation.result.decode()
+        operation = to_python_string(operation.result)
 
     if isinstance(operation, operation_pb2.operation):
-        return MessageToJson(operation).encode()
+        return to_cpp_string(MessageToJson(operation))
 
     if isinstance(operation, HF26Representation):
         operation = operation.dict(by_alias=True)
@@ -66,7 +67,7 @@ def prepare_operation_to_get_impacted_accounts(operation: OperationHF26) -> byte
     assert isinstance(operation, dict), "Operation must be dict at this moment."
 
     try:
-        formatted_operation = api_to_proto(json.dumps(operation).encode())
+        formatted_operation = api_to_proto(to_cpp_string(json.dumps(operation)))
         validate_wax_result(formatted_operation)
     except WaxValidationFailedError as error:
         raise InvalidOperationFormatError("Operation is not in the correct format.") from error
