7c95f0c69f7a217c0b748797de0601a23de971cf
kmochocki <kmochocki@syncad.com>
2024-12-18 13:06:45 +0000

    Move constants to separate file

--- python/wax/_private/core/constants.py
+++ python/wax/_private/core/constants.py
@@ -1,9 +1,20 @@
 from __future__ import annotations
 
+from datetime import timedelta
 from math import floor, log10
-from typing import Final
+from typing import TYPE_CHECKING, Final
+
+if TYPE_CHECKING:
+    from wax._private.models.basic import ChainId
+
 
 PUBLIC_KEY_ADDRESS_PREFIX: Final[str] = "STM"
 
+DEFAULT_TRANSACTION_EXPIRATION_TIME: Final[timedelta] = timedelta(minutes=1)
+MAINNET_CHAIN_ID: Final[ChainId] = "beeab0de00000000000000000000000000000000000000000000000000000000"
+DEFAULT_CHAIN_ID: Final[ChainId] = MAINNET_CHAIN_ID
+
 HIVE_PERCENT_PRECISION: Final[int] = 100
 HIVE_PERCENT_PRECISION_DOT_PLACES: Final[int] = floor(log10(HIVE_PERCENT_PRECISION))
+
+HIVE_TIME_FORMAT: Final[str] = "%Y-%m-%dT%H:%M:%S"
--- python/wax/wax_options.py
+++ python/wax/wax_options.py
@@ -1,12 +1,21 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
 
-from wax._private.models.basic import ChainId
+from wax._private.core.constants import DEFAULT_CHAIN_ID
+
+if TYPE_CHECKING:
+    from wax._private.models.basic import ChainId
 
 
-@dataclass
 class WaxOptions:
-    chain_id: ChainId = field(
-        default_factory=lambda: ChainId("18dcf0a285365fc58b71f18b3d3fec954aa0c141c44e4e5cb4cf777b9eab274e")
-    )
+    """Allows configuration of wax itself."""
+
+    def __init__(self, chain_id: ChainId = DEFAULT_CHAIN_ID) -> None:
+        """
+        Constructs WaxOptions.
+
+        Args:
+            chain_id: chain id used for signing. Defaults to mainnet chain id.
+        """
+        self.chain_id = chain_id
