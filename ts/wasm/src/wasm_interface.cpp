3d99cb0ec3b359f22bc589cfa33d3f2de9cb5a14
Bartek Wrona <wrona@syncad.com>
2024-12-19 13:51:45 +0100

    IAccountAuthorityProvider methods return std::optional values to correctly handle cases when source account does not exist

--- ts/wasm/src/wasm_interface.cpp
+++ ts/wasm/src/wasm_interface.cpp
@@ -151,11 +151,6 @@ json_asset cpp_estimate_hive_collateral( const json_price& current_median_histor
   return foundation::cpp_estimate_hive_collateral( current_median_history, current_min_history, hbd_amount_to_get );
 }
 
-wax_authority test_overrided_getAuthority(IAccountAuthorityProvider& provider, std::string account, std::string role)
-{
-  return provider.getAuthority(account, role);
-}
-
 };
 
 class AccountAuthorityProviderWrapper final : public emscripten::wrapper<IAccountAuthorityProvider>
@@ -163,12 +158,12 @@ class AccountAuthorityProviderWrapper final : public emscripten::wrapper<IAccoun
 public:
   EMSCRIPTEN_WRAPPER(AccountAuthorityProviderWrapper);
 
-  virtual wax_authority getAuthority(std::string account_name, std::string authorityRole) override
+  virtual std::optional<wax_authority> getAuthority(std::string account_name, std::string authorityRole) override
   {
     return call<wax_authority>("getAuthority", account_name, authorityRole);
   }
 
-  virtual std::string getWitnessPublicKey(std::string account_name) override
+  virtual std::optional<std::string> getWitnessPublicKey(std::string account_name) override
   {
     return call<std::string>("getWitnessPublicKey", account_name);
   }
@@ -346,8 +341,6 @@ EMSCRIPTEN_BINDINGS(wax_api_instance) {
     .function("cpp_hbd_to_hive", &foundation_wasm::cpp_hbd_to_hive)
     .function("cpp_hive_to_hbd", &foundation_wasm::cpp_hive_to_hbd)
     .function("cpp_estimate_hive_collateral", &foundation_wasm::cpp_estimate_hive_collateral)
-
-    .function("test_overrided_getAuthority", &foundation_wasm::test_overrided_getAuthority)
     ;
 
   class_<protocol_wasm, base<foundation_wasm>>("protocol")
