7af39e7bbbe072652a2f5d05258ee0aa173ef4c2
Krzysztof Mochocki <kmochocki@syncad.com>
2025-01-01 20:06:11 +0000

    Add fetching chain id and broadcasting transaction

--- examples/python/create_and_sign_transation.py
+++ examples/python/create_and_sign_transation.py
@@ -3,22 +3,29 @@ from wax import create_wax_foundation
 from wax.proto.asset_pb2 import asset
 from wax.proto.transfer_pb2 import transfer
 
+from helpy import Hived, Settings, HttpUrl
+
 PASSWORD = "pass"
 WALLET_NAME = "alice"
+HIVED_ADDRESS = HttpUrl("https://api.hive.blog")
+
+hived = Hived(settings=Settings(http_endpoint=HIVED_ADDRESS))
 
 wax = create_wax_foundation()
 keys = wax.suggest_brain_key()
 
-trx = wax.create_transaction_with_tapos("0576f3eb552fa3c7b8b103f1016c15ce2a259a0f")
+trx = wax.create_transaction_with_tapos(hived.api.database.get_config().CHAIN_ID)
 trx.push_operation(transfer(from_account="alice", to_account="bob", amount=asset(**wax.hbd.satoshis(10).dict())))
 
 
 with Beekeeper.factory() as beekeeper, beekeeper.create_session() as session, (
     session.create_wallet(name=WALLET_NAME, password=PASSWORD)
-    if WALLET_NAME not in [w.name for w in session.wallets_created]
-    else session.open_wallet(name=WALLET_NAME).unlock(PASSWORD)
-) as wallet:
-    wallet.import_key(private_key=keys.wif_private_key)
-    trx.sign(wallet=wallet, public_key=keys.associated_public_key)
+        if WALLET_NAME not in [w.name for w in session.wallets_created]
+            else session.open_wallet(name=WALLET_NAME).unlock(PASSWORD)
+            ) as wallet:
+                wallet.import_key(private_key=keys.wif_private_key)
+                    trx.sign(wallet=wallet, public_key=keys.associated_public_key)
 
-print(f"Signed transaction: {trx.to_api_json()}")
+                    print(f"broadcasting: {trx.to_api_json()}")
+                    hived.api.broadcast.broadcast_transaction(trx=trx.to_api.json())
+                    print("successfully broadcasted transaction")
