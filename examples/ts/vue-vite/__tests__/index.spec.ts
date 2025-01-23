0b3908dbc2f7f1ff04fe8d57410ffec15f14539d
Bartek Wrona <wrona@syncad.com>
2025-01-24 00:13:32 +0100

    Implemented waiting for server with retries

--- examples/ts/vue-vite/__tests__/index.spec.ts
+++ examples/ts/vue-vite/__tests__/index.spec.ts
@@ -1,5 +1,40 @@
 import { expect, test } from '@playwright/test';
 
+const waitForServerReady = async (url: string, interval: number = 100, attempts: number = 10): Promise<void> => {
+  console.log(`Awaiting a server: ${url}...`);
+
+  for (let count = 0; count < attempts; ++count) {
+    try {
+      console.log(`Trying to connect ${count}/${attempts})`);
+
+      const response = await fetch(url, {
+        method: "GET",
+        signal: AbortSignal.timeout(interval)
+      });
+
+      if (response.ok && response.status === 200) {
+        console.log(`Connected successfully (${count}/${attempts}). Exiting.`);
+        return;
+      }
+    }
+    catch (error) {
+      if (typeof error !== "object" || !(error instanceof Error))
+        console.log(`Caught an UNKNOWN error (${JSON.stringify(error)})`);
+      else
+      if (error.name === "TimeoutError")
+        console.log(`Caught a TIMEOUT error (${JSON.stringify(error)})`);
+      else
+      if (error.name === "AbortError")
+        console.log(`Caught an ABORT error (${JSON.stringify(error)})`);
+    }
+
+    console.log(`Waiting for ${interval} ms...)`);
+    await new Promise(resolve => { setTimeout(resolve, interval); });
+  }
+
+  console.log(`Still down - bailing out.`);
+};
+
 test.describe('Proper WASM Wax loading on playwright ', () => {
   test.beforeEach(async ({ page }) => {
     /// use >> marker for each texts printed in the browser context
@@ -7,8 +42,7 @@ test.describe('Proper WASM Wax loading on playwright ', () => {
       console.log('>>', msg.type(), msg.text());
     });
 
-    // Wait ~500ms for the Vite server to warm up
-    await new Promise(resolve => { setTimeout(resolve, 500); });
+    await waitForServerReady("http://localhost:5173");
 
     await page.goto("http://localhost:5173", { waitUntil: "load" });
   });
