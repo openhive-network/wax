91b4c9e3ff3a616dda9d2a1ff965007ac7eff213
mtyszczak <mateusz.tyszczak@gmail.com>
2025-02-12 16:01:55 +0100

    Fix proper file dependency path matching for parcel

--- ts/wasm/lib/index.ts
+++ ts/wasm/lib/index.ts
@@ -14,7 +14,7 @@ const ENVIRONMENT_IS_WORKER = typeof WorkerGlobalScope != 'undefined';
 const getModuleExt = () => ({
   locateFile: (path, scriptDirectory) => {
     if (path === "wax.common.wasm")
-        return new URL("./build_wasm/wax.common.wasm", ENVIRONMENT_IS_WORKER ? self.location.href : import.meta.url).href;
+        return (ENVIRONMENT_IS_WORKER ? new URL("./build_wasm/wax.common.wasm", self.location.href) : new URL("./build_wasm/wax.common.wasm",  import.meta.url)).href;
     return scriptDirectory + path;
   }
 })
