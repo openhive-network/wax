6391cd7419dae1869dbb44db3796dddf7a92692b
Jakub Ziebinski <ziebinskijakub@gmail.com>
2025-03-03 10:09:18 +0100

    Removal of synchronus transaction signing

--- examples/python/create_and_sign_transation.py
+++ examples/python/create_and_sign_transation.py
@@ -1,31 +1,31 @@
-from beekeepy import Beekeeper
+import asyncio
+
+from beekeepy import AsyncBeekeeper
+from beekeepy.interfaces import HttpUrl
 from wax import create_wax_foundation
-from wax.proto.asset_pb2 import asset
 from wax.proto.transfer_pb2 import transfer
 
-from helpy import Hived, Settings, HttpUrl
 
 PASSWORD = "pass"
 WALLET_NAME = "alice"
 HIVED_ADDRESS = HttpUrl("https://api.hive.blog")
 
-hived = Hived(settings=Settings(http_endpoint=HIVED_ADDRESS))
-
 wax = create_wax_foundation()
 keys = wax.suggest_brain_key()
 
-trx = wax.create_transaction_with_tapos(hived.api.database.get_config().CHAIN_ID)
-trx.push_operation(transfer(from_account="alice", to_account="bob", amount=asset(**wax.hbd.satoshis(10).dict())))
+trx = wax.create_transaction_with_tapos("00000449f7860b82b4fbe2f317c670e9f01d6d9a")
+trx.push_operation(transfer(from_account="alice", to_account="bob", amount=wax.hive.satoshis(10)))
 
 
-with Beekeeper.factory() as beekeeper, beekeeper.create_session() as session, (
-    session.create_wallet(name=WALLET_NAME, password=PASSWORD)
-        if WALLET_NAME not in [w.name for w in session.wallets_created]
-            else session.open_wallet(name=WALLET_NAME).unlock(PASSWORD)
-            ) as wallet:
-                wallet.import_key(private_key=keys.wif_private_key)
-                    trx.sign(wallet=wallet, public_key=keys.associated_public_key)
+async def sign_trx():
+    async with await AsyncBeekeeper.factory() as beekeeper:
+        async with await beekeeper.create_session() as session:
+            wallet = await session.create_wallet(name=WALLET_NAME, password=PASSWORD)
+            await wallet.import_key(private_key=keys.wif_private_key)
+            await trx.sign(wallet=wallet, public_key=keys.associated_public_key)
+            print(f"broadcasting: {trx.to_api_json()}")
 
-                    print(f"broadcasting: {trx.to_api_json()}")
-                    hived.api.broadcast.broadcast_transaction(trx=trx.to_api.json())
-                    print("successfully broadcasted transaction")
+async def main():
+    await sign_trx()
+
+asyncio.run(main())
