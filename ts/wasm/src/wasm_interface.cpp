02e6bbb07a5867429d763ab7e6785101c7d68188
Bartek Wrona <wrona@syncad.com>
2025-03-04 23:37:16 +0100

    cpp_convert_raw_private_key_to_wif exposed to WASM

--- ts/wasm/src/wasm_interface.cpp
+++ ts/wasm/src/wasm_interface.cpp
@@ -95,6 +95,11 @@ result cpp_generate_private_key()
 private_key_data cpp_generate_private_key_password_based(const std::string& account, const std::string& role, const std::string& password)
 { return foundation::cpp_generate_private_key(account, role, password); }
 
+std::string cpp_convert_raw_private_key_to_wif(const std::string& hexData)
+{
+  return foundation::cpp_convert_raw_private_key_to_wif(hexData);
+}
+
 result cpp_get_public_key_from_signature(const std::string& digest, const std::string& signature)
 { return foundation::cpp_get_public_key_from_signature(digest, signature); }
 
@@ -306,6 +311,7 @@ EMSCRIPTEN_BINDINGS(wax_api_instance) {
 
     .function("cpp_generate_private_key", &foundation_wasm::cpp_generate_private_key)
     .function("cpp_generate_private_key_password_based", &foundation_wasm::cpp_generate_private_key_password_based)
+    .function("cpp_convert_raw_private_key_to_wif", &foundation_wasm::cpp_convert_raw_private_key_to_wif)
     .function("cpp_get_public_key_from_signature", &foundation_wasm::cpp_get_public_key_from_signature)
 
     // Based on https://emscripten.org/docs/porting/connecting_cpp_and_javascript/embind.html#overloaded-functions:
