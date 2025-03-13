10f05c3f348c475d797ad361cb47c80edb324f03
Bartek Wrona <wrona@syncad.com>
2025-03-13 23:30:41 +0100

    Browser setup moved to the playwright config

--- examples/ts/signature-extension/playwright.config.ts
+++ examples/ts/signature-extension/playwright.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig, devices } from '@playwright/test';
 
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToExtension = path.join(__dirname, "./__tests__/extensions/Hive-Keychain");
+
+console.log(`Effective extension directory: ${pathToExtension}`);
+
+
 export default defineConfig({
   testDir: './__tests__',
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'keychain signing tests',
+      use: { ...devices['Desktop Chrome'], channel: 'chromium',
+        launchOptions: {
+          args: [
+            `--load-extension=${pathToExtension}`
+            ,`--disable-extensions-except=${pathToExtension}`
+          ],
+          ignoreDefaultArgs: ['--disable-component-extensions-with-background-pages'],
+        },
+      },
     },
   ],
 
