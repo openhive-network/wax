3ad203b54bc887d6c14108d8e9d73adcc7347c7c
Jakub Ziebinski <ziebinskijakub@gmail.com>
2025-03-18 12:41:27 +0100

    Run pre-commit in the wax_visitor.py

--- python/wax/wax_visitor.py
+++ python/wax/wax_visitor.py
@@ -1,405 +1,404 @@
 from abc import ABC, abstractmethod
 
-from wax.proto import (
-    vote_pb2,
-    comment_pb2,
-    transfer_pb2,
-    transfer_to_vesting_pb2,
-    withdraw_vesting_pb2,
-    limit_order_create_pb2,
-    limit_order_cancel_pb2,
-    feed_publish_pb2,
-    convert_pb2,
-    account_create_pb2,
-    account_update_pb2,
-    witness_update_pb2,
-    account_witness_vote_pb2,
-    account_witness_proxy_pb2,
-    pow_pb2,
-    custom_pb2,
-    witness_block_approve_pb2,
-    delete_comment_pb2,
-    custom_json_pb2,
-    comment_options_pb2,
-    set_withdraw_vesting_route_pb2,
-    limit_order_create2_pb2,
-    claim_account_pb2,
-    create_claimed_account_pb2,
-    request_account_recovery_pb2,
-    recover_account_pb2,
-    change_recovery_account_pb2,
-    escrow_transfer_pb2,
-    escrow_dispute_pb2,
-    escrow_release_pb2,
-    pow2_pb2,
-    escrow_approve_pb2,
-    transfer_to_savings_pb2,
-    transfer_from_savings_pb2,
-    cancel_transfer_from_savings_pb2,
-    decline_voting_rights_pb2,
-    claim_reward_balance_pb2,
-    delegate_vesting_shares_pb2,
-    account_create_with_delegation_pb2,
-    witness_set_properties_pb2,
-    account_update2_pb2,
-    create_proposal_pb2,
-    update_proposal_votes_pb2,
-    remove_proposal_pb2,
-    update_proposal_pb2,
-    collateralized_convert_pb2,
-    recurrent_transfer_pb2,
-    operation_pb2,
-    transaction_pb2
+from wax.proto.operations import (
+    account_create,
+    account_create_with_delegation,
+    account_update,
+    account_update2,
+    account_witness_proxy,
+    account_witness_vote,
+    cancel_transfer_from_savings,
+    change_recovery_account,
+    claim_account,
+    claim_reward_balance,
+    collateralized_convert,
+    comment,
+    comment_options,
+    convert,
+    create_claimed_account,
+    create_proposal,
+    custom,
+    custom_json,
+    decline_voting_rights,
+    delegate_vesting_shares,
+    delete_comment,
+    escrow_approve,
+    escrow_dispute,
+    escrow_release,
+    escrow_transfer,
+    feed_publish,
+    limit_order_cancel,
+    limit_order_create,
+    limit_order_create2,
+    operation,
+    pow,
+    pow2,
+    recover_account,
+    recurrent_transfer,
+    remove_proposal,
+    request_account_recovery,
+    set_withdraw_vesting_route,
+    transfer,
+    transfer_from_savings,
+    transfer_to_savings,
+    transfer_to_vesting,
+    update_proposal,
+    update_proposal_votes,
+    vote,
+    withdraw_vesting,
+    witness_block_approve,
+    witness_set_properties,
+    witness_update,
 )
 
 
 class AbstractOperationVisitor(ABC):
-    """
-    User must overrides all the abstract methods in derived visitor class.
-    """
+    """User must overrides all the abstract methods in derived visitor class."""
 
-    def accept(self, operation: operation_pb2.operation):
-        target_operation_name = operation.WhichOneof("value")
+    def accept(self, op: operation) -> None:
+        target_operation_name = op.WhichOneof("value")
         if hasattr(self, target_operation_name):
             method = getattr(self, target_operation_name)
-            target_operation = getattr(operation, target_operation_name)
+            target_operation = getattr(op, target_operation_name)
             method(target_operation)
         else:
-            raise AttributeError("Method for operation '{}' not exists in '{}'.".format(target_operation_name, __class__.__name__))
+            raise AttributeError(
+                f"Method for operation '{target_operation_name}' not exists in '{self.__class__.__name__}'."
+            )
 
     @abstractmethod
-    def vote(self, op:vote_pb2.vote):
+    def vote(self, op: vote) -> None:
         pass
 
     @abstractmethod
-    def comment(self, op:comment_pb2.comment):
+    def comment(self, op: comment) -> None:
         pass
 
     @abstractmethod
-    def transfer(self, op:transfer_pb2.transfer):
+    def transfer(self, op: transfer) -> None:
         pass
 
     @abstractmethod
-    def transfer_to_vesting(self, op:transfer_to_vesting_pb2.transfer_to_vesting):
+    def transfer_to_vesting(self, op: transfer_to_vesting) -> None:
         pass
 
     @abstractmethod
-    def withdraw_vesting(self, op:withdraw_vesting_pb2.withdraw_vesting):
+    def withdraw_vesting(self, op: withdraw_vesting) -> None:
         pass
 
     @abstractmethod
-    def limit_order_create(self, op:limit_order_create_pb2.limit_order_create):
+    def limit_order_create(self, op: limit_order_create) -> None:
         pass
 
     @abstractmethod
-    def limit_order_cancel(self, op:limit_order_cancel_pb2.limit_order_cancel):
+    def limit_order_cancel(self, op: limit_order_cancel) -> None:
         pass
 
     @abstractmethod
-    def feed_publish(self, op:feed_publish_pb2.feed_publish):
+    def feed_publish(self, op: feed_publish) -> None:
         pass
 
     @abstractmethod
-    def convert(self, op:convert_pb2.convert):
+    def convert(self, op: convert) -> None:
         pass
 
     @abstractmethod
-    def account_create(self, op:account_create_pb2.account_create):
+    def account_create(self, op: account_create) -> None:
         pass
 
     @abstractmethod
-    def account_update(self, op:account_update_pb2.account_update):
+    def account_update(self, op: account_update) -> None:
         pass
 
     @abstractmethod
-    def witness_update(self, op:witness_update_pb2.witness_update):
+    def witness_update(self, op: witness_update) -> None:
         pass
 
     @abstractmethod
-    def account_witness_vote(self, op:account_witness_vote_pb2.account_witness_vote):
+    def account_witness_vote(self, op: account_witness_vote) -> None:
         pass
 
     @abstractmethod
-    def account_witness_proxy(self, op:account_witness_proxy_pb2.account_witness_proxy):
+    def account_witness_proxy(self, op: account_witness_proxy) -> None:
         pass
 
     @abstractmethod
-    def pow(self, op:pow_pb2.pow):
+    def pow(self, op: pow) -> None:
         pass
 
     @abstractmethod
-    def custom(self, op:custom_pb2.custom):
+    def custom(self, op: custom) -> None:
         pass
 
     @abstractmethod
-    def witness_block_approve(self, op:witness_block_approve_pb2.witness_block_approve):
+    def witness_block_approve(self, op: witness_block_approve) -> None:
         pass
 
     @abstractmethod
-    def delete_comment(self, op:delete_comment_pb2.delete_comment):
+    def delete_comment(self, op: delete_comment) -> None:
         pass
 
     @abstractmethod
-    def custom_json(self, op:custom_json_pb2.custom_json):
+    def custom_json(self, op: custom_json) -> None:
         pass
 
     @abstractmethod
-    def comment_options(self, op:comment_options_pb2.comment_options):
+    def comment_options(self, op: comment_options) -> None:
         pass
 
     @abstractmethod
-    def set_withdraw_vesting_route(self, op:set_withdraw_vesting_route_pb2.set_withdraw_vesting_route):
+    def set_withdraw_vesting_route(self, op: set_withdraw_vesting_route) -> None:
         pass
 
     @abstractmethod
-    def limit_order_create2(self, op:limit_order_create2_pb2.limit_order_create2):
+    def limit_order_create2(self, op: limit_order_create2) -> None:
         pass
 
     @abstractmethod
-    def claim_account(self, op:claim_account_pb2.claim_account):
+    def claim_account(self, op: claim_account) -> None:
         pass
 
     @abstractmethod
-    def create_claimed_account(self, op:create_claimed_account_pb2.create_claimed_account):
+    def create_claimed_account(self, op: create_claimed_account) -> None:
         pass
 
     @abstractmethod
-    def request_account_recovery(self, op:request_account_recovery_pb2.request_account_recovery):
+    def request_account_recovery(self, op: request_account_recovery) -> None:
         pass
 
     @abstractmethod
-    def recover_account(self, op:recover_account_pb2.recover_account):
+    def recover_account(self, op: recover_account) -> None:
         pass
 
     @abstractmethod
-    def change_recovery_account(self, op:change_recovery_account_pb2.change_recovery_account):
+    def change_recovery_account(self, op: change_recovery_account) -> None:
         pass
 
     @abstractmethod
-    def escrow_transfer(self, op:escrow_transfer_pb2.escrow_transfer):
+    def escrow_transfer(self, op: escrow_transfer) -> None:
         pass
 
     @abstractmethod
-    def escrow_dispute(self, op:escrow_dispute_pb2.escrow_dispute):
+    def escrow_dispute(self, op: escrow_dispute) -> None:
         pass
 
     @abstractmethod
-    def escrow_release(self, op:escrow_release_pb2.escrow_release):
+    def escrow_release(self, op: escrow_release) -> None:
         pass
 
     @abstractmethod
-    def pow2(self, op:pow2_pb2.pow2):
+    def pow2(self, op: pow2) -> None:
         pass
 
     @abstractmethod
-    def escrow_approve(self, op:escrow_approve_pb2.escrow_approve):
+    def escrow_approve(self, op: escrow_approve) -> None:
         pass
 
     @abstractmethod
-    def transfer_to_savings(self, op:transfer_to_savings_pb2.transfer_to_savings):
+    def transfer_to_savings(self, op: transfer_to_savings) -> None:
         pass
 
     @abstractmethod
-    def transfer_from_savings(self, op:transfer_from_savings_pb2.transfer_from_savings):
+    def transfer_from_savings(self, op: transfer_from_savings) -> None:
         pass
 
     @abstractmethod
-    def cancel_transfer_from_savings(self, op:cancel_transfer_from_savings_pb2.cancel_transfer_from_savings):
+    def cancel_transfer_from_savings(self, op: cancel_transfer_from_savings) -> None:
         pass
 
     @abstractmethod
-    def decline_voting_rights(self, op:decline_voting_rights_pb2.decline_voting_rights):
+    def decline_voting_rights(self, op: decline_voting_rights) -> None:
         pass
 
     @abstractmethod
-    def claim_reward_balance(self, op:claim_reward_balance_pb2.claim_reward_balance):
+    def claim_reward_balance(self, op: claim_reward_balance) -> None:
         pass
 
     @abstractmethod
-    def delegate_vesting_shares(self, op:delegate_vesting_shares_pb2.delegate_vesting_shares):
+    def delegate_vesting_shares(self, op: delegate_vesting_shares) -> None:
         pass
 
     @abstractmethod
-    def account_create_with_delegation(self, op:account_create_with_delegation_pb2.account_create_with_delegation):
+    def account_create_with_delegation(self, op: account_create_with_delegation) -> None:
         pass
 
     @abstractmethod
-    def witness_set_properties(self, op:witness_set_properties_pb2.witness_set_properties):
+    def witness_set_properties(self, op: witness_set_properties) -> None:
         pass
 
     @abstractmethod
-    def account_update2(self, op:account_update2_pb2.account_update2):
+    def account_update2(self, op: account_update2) -> None:
         pass
 
     @abstractmethod
-    def create_proposal(self, op:create_proposal_pb2.create_proposal):
+    def create_proposal(self, op: create_proposal) -> None:
         pass
 
     @abstractmethod
-    def update_proposal_votes(self, op:update_proposal_votes_pb2.update_proposal_votes):
+    def update_proposal_votes(self, op: update_proposal_votes) -> None:
         pass
 
     @abstractmethod
-    def remove_proposal(self, op:remove_proposal_pb2.remove_proposal):
+    def remove_proposal(self, op: remove_proposal) -> None:
         pass
 
     @abstractmethod
-    def update_proposal(self, op:update_proposal_pb2.update_proposal):
+    def update_proposal(self, op: update_proposal) -> None:
         pass
 
     @abstractmethod
-    def collateralized_convert(self, op:collateralized_convert_pb2.collateralized_convert):
+    def collateralized_convert(self, op: collateralized_convert) -> None:
         pass
 
     @abstractmethod
-    def recurrent_transfer(self, op:recurrent_transfer_pb2.recurrent_transfer):
+    def recurrent_transfer(self, op: recurrent_transfer) -> None:
         pass
-
 
 
 class OperationVisitor(AbstractOperationVisitor):
     """
-    User need to overrides only method(s) for operation(s) he want be processed.
+    User need to override only method(s) for operation(s) he wants to be processed.
+
     All other operations will be silently skipped.
     """
 
-    def vote(self, op:vote_pb2.vote):
+    def vote(self, op: vote) -> None:
         pass
 
-    def comment(self, op:comment_pb2.comment):
+    def comment(self, op: comment) -> None:
         pass
 
-    def transfer(self, op:transfer_pb2.transfer):
+    def transfer(self, op: transfer) -> None:
         pass
 
-    def transfer_to_vesting(self, op:transfer_to_vesting_pb2.transfer_to_vesting):
+    def transfer_to_vesting(self, op: transfer_to_vesting) -> None:
         pass
 
-    def withdraw_vesting(self, op:withdraw_vesting_pb2.withdraw_vesting):
+    def withdraw_vesting(self, op: withdraw_vesting) -> None:
         pass
 
-    def limit_order_create(self, op:limit_order_create_pb2.limit_order_create):
+    def limit_order_create(self, op: limit_order_create) -> None:
         pass
 
-    def limit_order_cancel(self, op:limit_order_cancel_pb2.limit_order_cancel):
+    def limit_order_cancel(self, op: limit_order_cancel) -> None:
         pass
 
-    def feed_publish(self, op:feed_publish_pb2.feed_publish):
+    def feed_publish(self, op: feed_publish) -> None:
         pass
 
-    def convert(self, op:convert_pb2.convert):
+    def convert(self, op: convert) -> None:
         pass
 
-    def account_create(self, op:account_create_pb2.account_create):
+    def account_create(self, op: account_create) -> None:
         pass
 
-    def account_update(self, op:account_update_pb2.account_update):
+    def account_update(self, op: account_update) -> None:
         pass
 
-    def witness_update(self, op:witness_update_pb2.witness_update):
+    def witness_update(self, op: witness_update) -> None:
         pass
 
-    def account_witness_vote(self, op:account_witness_vote_pb2.account_witness_vote):
+    def account_witness_vote(self, op: account_witness_vote) -> None:
         pass
 
-    def account_witness_proxy(self, op:account_witness_proxy_pb2.account_witness_proxy):
+    def account_witness_proxy(self, op: account_witness_proxy) -> None:
         pass
 
-    def pow(self, op:pow_pb2.pow):
+    def pow(self, op: pow) -> None:
         pass
 
-    def custom(self, op:custom_pb2.custom):
+    def custom(self, op: custom) -> None:
         pass
 
-    def witness_block_approve(self, op:witness_block_approve_pb2.witness_block_approve):
+    def witness_block_approve(self, op: witness_block_approve) -> None:
         pass
 
-    def delete_comment(self, op:delete_comment_pb2.delete_comment):
+    def delete_comment(self, op: delete_comment) -> None:
         pass
 
-    def custom_json(self, op:custom_json_pb2.custom_json):
+    def custom_json(self, op: custom_json) -> None:
         pass
 
-    def comment_options(self, op:comment_options_pb2.comment_options):
+    def comment_options(self, op: comment_options) -> None:
         pass
 
-    def set_withdraw_vesting_route(self, op:set_withdraw_vesting_route_pb2.set_withdraw_vesting_route):
+    def set_withdraw_vesting_route(self, op: set_withdraw_vesting_route) -> None:
         pass
 
-    def limit_order_create2(self, op:limit_order_create2_pb2.limit_order_create2):
+    def limit_order_create2(self, op: limit_order_create2) -> None:
         pass
 
-    def claim_account(self, op:claim_account_pb2.claim_account):
+    def claim_account(self, op: claim_account) -> None:
         pass
 
-    def create_claimed_account(self, op:create_claimed_account_pb2.create_claimed_account):
+    def create_claimed_account(self, op: create_claimed_account) -> None:
         pass
 
-    def request_account_recovery(self, op:request_account_recovery_pb2.request_account_recovery):
+    def request_account_recovery(self, op: request_account_recovery) -> None:
         pass
 
-    def recover_account(self, op:recover_account_pb2.recover_account):
+    def recover_account(self, op: recover_account) -> None:
         pass
 
-    def change_recovery_account(self, op:change_recovery_account_pb2.change_recovery_account):
+    def change_recovery_account(self, op: change_recovery_account) -> None:
         pass
 
-    def escrow_transfer(self, op:escrow_transfer_pb2.escrow_transfer):
+    def escrow_transfer(self, op: escrow_transfer) -> None:
         pass
 
-    def escrow_dispute(self, op:escrow_dispute_pb2.escrow_dispute):
+    def escrow_dispute(self, op: escrow_dispute) -> None:
         pass
 
-    def escrow_release(self, op:escrow_release_pb2.escrow_release):
+    def escrow_release(self, op: escrow_release) -> None:
         pass
 
-    def pow2(self, op:pow2_pb2.pow2):
+    def pow2(self, op: pow2) -> None:
         pass
 
-    def escrow_approve(self, op:escrow_approve_pb2.escrow_approve):
+    def escrow_approve(self, op: escrow_approve) -> None:
         pass
 
-    def transfer_to_savings(self, op:transfer_to_savings_pb2.transfer_to_savings):
+    def transfer_to_savings(self, op: transfer_to_savings) -> None:
         pass
 
-    def transfer_from_savings(self, op:transfer_from_savings_pb2.transfer_from_savings):
+    def transfer_from_savings(self, op: transfer_from_savings) -> None:
         pass
 
-    def cancel_transfer_from_savings(self, op:cancel_transfer_from_savings_pb2.cancel_transfer_from_savings):
+    def cancel_transfer_from_savings(self, op: cancel_transfer_from_savings) -> None:
         pass
 
-    def decline_voting_rights(self, op:decline_voting_rights_pb2.decline_voting_rights):
+    def decline_voting_rights(self, op: decline_voting_rights) -> None:
         pass
 
-    def claim_reward_balance(self, op:claim_reward_balance_pb2.claim_reward_balance):
+    def claim_reward_balance(self, op: claim_reward_balance) -> None:
         pass
 
-    def delegate_vesting_shares(self, op:delegate_vesting_shares_pb2.delegate_vesting_shares):
+    def delegate_vesting_shares(self, op: delegate_vesting_shares) -> None:
         pass
 
-    def account_create_with_delegation(self, op:account_create_with_delegation_pb2.account_create_with_delegation):
+    def account_create_with_delegation(self, op: account_create_with_delegation) -> None:
         pass
 
-    def witness_set_properties(self, op:witness_set_properties_pb2.witness_set_properties):
+    def witness_set_properties(self, op: witness_set_properties) -> None:
         pass
 
-    def account_update2(self, op:account_update2_pb2.account_update2):
+    def account_update2(self, op: account_update2) -> None:
         pass
 
-    def create_proposal(self, op:create_proposal_pb2.create_proposal):
+    def create_proposal(self, op: create_proposal) -> None:
         pass
 
-    def update_proposal_votes(self, op:update_proposal_votes_pb2.update_proposal_votes):
+    def update_proposal_votes(self, op: update_proposal_votes) -> None:
         pass
 
-    def remove_proposal(self, op:remove_proposal_pb2.remove_proposal):
+    def remove_proposal(self, op: remove_proposal) -> None:
         pass
 
-    def update_proposal(self, op:update_proposal_pb2.update_proposal):
+    def update_proposal(self, op: update_proposal) -> None:
         pass
 
-    def collateralized_convert(self, op:collateralized_convert_pb2.collateralized_convert):
+    def collateralized_convert(self, op: collateralized_convert) -> None:
         pass
 
-    def recurrent_transfer(self, op:recurrent_transfer_pb2.recurrent_transfer):
+    def recurrent_transfer(self, op: recurrent_transfer) -> None:
         pass
