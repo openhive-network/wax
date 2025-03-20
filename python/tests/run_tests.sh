751bb00ab3e42190c2e1ef621dd5cf09e7007c0a
Bartek Wrona <wrona@syncad.com>
2025-03-20 21:47:33 +0100

    python/tests/run_tests.sh allows to pass additional parameters

--- python/tests/run_tests.sh
+++ python/tests/run_tests.sh
@@ -3,4 +3,6 @@ set -e
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-poetry -C "${SCRIPTPATH}/../" run pytest --junitxml="${SCRIPTPATH}/report.xml" --ignore="${SCRIPTPATH}/simple_flow" --ignore="${SCRIPTPATH}/helpy_test" "${SCRIPTPATH}"
+# Allow to pass additional parameters to the underlying pytest command, i.e. -vvv ./tests/operation_visitor to run only single group
+
+poetry -C "${SCRIPTPATH}/../" run pytest --junitxml="${SCRIPTPATH}/report.xml" --ignore="${SCRIPTPATH}/simple_flow" --ignore="${SCRIPTPATH}/helpy_test" "${SCRIPTPATH}" "$@"
