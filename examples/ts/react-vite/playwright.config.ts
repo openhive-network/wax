13dea6f72a9c4ced029f59e3be1e92e748168cc2
Bartek Wrona <wrona@syncad.com>
2025-03-25 15:50:37 +0100

    Added timeout to webserver spawned by Playwright during execution of examples:react-vite example

--- examples/ts/react-vite/playwright.config.ts
+++ examples/ts/react-vite/playwright.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
   ],
   // Run your local dev server before starting the tests
   webServer: {
-    command: 'npm run dev'
+    command: 'npm run dev',
+    "url": "http://localhost:5173",
+    "timeout": 120 * 1000,
+    "reuseExistingServer": false
   }
 });
