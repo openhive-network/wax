41b95ba6ab867b1925341ab5b94293f86387b2af
mtyszczak <mateusz.tyszczak@gmail.com>
2025-05-07 16:10:21 +0200

    Python test pure tx: retrieve underlying operation type

--- python/test_pure_tx.py
+++ python/test_pure_tx.py
@@ -1,5 +1,5 @@
 from wax import create_wax_foundation, cpp_pass_pure_transaction
-from wax.proto.operations import comment as comment_operation, operation
+from wax.proto.operations import comment, operation
 from wax.proto.transaction import transaction as proto_transaction
 
 from google.protobuf.json_format import MessageToDict
@@ -12,6 +12,16 @@ transaction = waxF.create_transaction_with_tapos(TAPOS)
 
 tx = transaction.transaction
 
+tx.operations.append(operation(comment_operation = comment(
+  author="test",
+  permlink="test",
+  parent_author="test",
+  parent_permlink="test",
+  title="test",
+  body="test",
+  json_metadata="{}"
+)))
+
 txc = proto_transaction(expiration = tx.expiration, ref_block_num=tx.ref_block_num, ref_block_prefix=tx.ref_block_prefix, operations=tx.operations, extensions=tx.extensions);
 
 tx2 = MessageToDict(tx, True)
@@ -22,5 +32,11 @@ for(key, value) in tx2.items():
 cpp_pass_pure_transaction(tx)
 
 print("===================================")
+
+print(f"tx expiration: {txc.expiration}")
+
+op_type = tx.operations[0].WhichOneof("value")
+op = tx.operations[0].__getattribute__(op_type)
+print(f"op_type: {op_type}")
 
 cpp_pass_pure_transaction(txc)
