510f69b53ef0c271fb68ceaf78d6a6b4a190a967
mtyszczak <mateusz.tyszczak@gmail.com>
2025-05-08 13:21:06 +0200

    Create a separate class interface to be used with WasmTransaction compatible methods

--- ts/wasm/src/val_protocol.hpp
+++ ts/wasm/src/val_protocol.hpp
@@ -2,19 +2,19 @@
 
 #include <hive/protocol/transaction.hpp>
 #include <fc/static_variant.hpp>
-#include <emscripten/val.h>
 #include "core/binary_view/node_types.hpp"
 #include "core/binary_view/traits.hpp"
 
 namespace cpp {
 
 namespace {
+template<typename ManagedObjectT>
 class val_to_static_variant
 {
 public:
   using result_type = void;
 
-  val_to_static_variant(emscripten::val jsval, bool is_protobuf)
+  val_to_static_variant(ManagedObjectT jsval, bool is_protobuf)
     : jsval(jsval), is_protobuf(is_protobuf)
   {}
 
@@ -22,14 +22,14 @@ public:
   result_type operator()( T& v )const;
 
 private:
-  emscripten::val jsval;
+  ManagedObjectT jsval;
   bool is_protobuf;
 };
 }
 
 
-template< typename... Ts >
-void from_jsval( emscripten::val jsval, fc::static_variant< Ts... >& v, bool is_protobuf )
+template< typename ManagedObjectT, typename... Ts >
+void from_jsval( ManagedObjectT jsval, fc::static_variant< Ts... >& v, bool is_protobuf )
 {
   static std::map< std::string, int64_t > to_tag = []()
   {
@@ -46,20 +46,15 @@ void from_jsval( emscripten::val jsval, fc::static_variant< Ts... >& v, bool is_
 
   int64_t which = -1;
 
-  emscripten::val nextval;
+  ManagedObjectT nextval;
 
   if (is_protobuf)
   {
-    emscripten::val keys = emscripten::val::global("Object").call<emscripten::val>("keys", jsval);
-    uint32_t count = keys["length"].as<uint32_t>();
-
-    for (uint32_t i = 0; i < count; ++i)
+    for (const auto& key : jsval.keys())
     {
-      std::string key = keys[i].as<std::string>();
+      ManagedObjectT el = jsval[key];
 
-      emscripten::val el = jsval[key];
-
-      if (el.isUndefined())
+      if (el.is_undefined())
         continue;
 
       const auto it = to_tag.find(key);
@@ -75,7 +70,7 @@ void from_jsval( emscripten::val jsval, fc::static_variant< Ts... >& v, bool is_
   }
   else
   {
-    std::string type = jsval["type"].as<std::string>();
+    std::string type = jsval["type"].template as<std::string>();
 
     auto itr = to_tag.find( type );
     FC_ASSERT( itr != to_tag.end(), "Invalid object name: ${n}", ("n", type) );
@@ -89,17 +84,17 @@ void from_jsval( emscripten::val jsval, fc::static_variant< Ts... >& v, bool is_
   v = tmp;
 }
 
-template< typename T >
+template< typename ManagedObjectT, typename T >
 class val_protocol_visitor {
 public:
-  val_protocol_visitor( emscripten::val jsval, T& val, bool is_protobuf )
+  val_protocol_visitor( ManagedObjectT jsval, T& val, bool is_protobuf )
     : jsval( jsval ), is_protobuf( is_protobuf ), val( val )
   {}
 
   template< typename Member, class Class, Member( Class::*member ) >
   void operator()( const char* name ) const
   {
-    if (jsval[name].isUndefined())
+    if (jsval[name].is_undefined())
       return;
 
     this->add( name, val.*member );
@@ -116,7 +111,7 @@ public:
 
   void add( const char* name, hive::protocol::asset& v ) const
   {
-    emscripten::val amount = jsval[name];
+    ManagedObjectT amount = jsval[name];
     v.amount = boost::lexical_cast< int64_t >( amount[ "amount" ].template as< std::string >() );
     v.symbol = hive::protocol::asset_symbol_type::from_nai_string(
       amount[ "nai" ].template as< std::string >().c_str(),
@@ -126,34 +121,34 @@ public:
 
   void add( const char* name, hive::protocol::price& v ) const
   {
-    val_protocol_visitor< hive::protocol::price > visitor{ jsval[name], v, is_protobuf };
+    val_protocol_visitor< ManagedObjectT, hive::protocol::price > visitor{ jsval[name], v, is_protobuf };
     visitor.add( "base", v.base );
     visitor.add( "quote", v.quote );
   }
 
   void add( const char* name, hive::protocol::json_string& v ) const
   {
-    v = hive::protocol::json_string{ jsval[name].as<std::string>() };
+    v = hive::protocol::json_string{ jsval[name].template as<std::string>() };
   }
 
   void add( const char* name, fc::ripemd160& v ) const
   {
-    v = fc::ripemd160{ jsval[name].as<std::string>() };
+    v = fc::ripemd160{ jsval[name].template as<std::string>() };
   }
 
   void add( const char* name, fc::sha256& v ) const
   {
-    v = fc::sha256{ jsval[name].as<std::string>() };
+    v = fc::sha256{ jsval[name].template as<std::string>() };
   }
 
   void add( const char* name, hive::protocol::public_key_type& v ) const
   {
-    v = hive::protocol::public_key_type{ jsval[name].as<std::string>() };
+    v = hive::protocol::public_key_type{ jsval[name].template as<std::string>() };
   }
 
   void add( const char* name, hive::protocol::authority& v ) const
   {
-    val_protocol_visitor< hive::protocol::authority > visitor{ jsval[name], v, is_protobuf };
+    val_protocol_visitor< ManagedObjectT, hive::protocol::authority > visitor{ jsval[name], v, is_protobuf };
 
     visitor.add( "weight_threshold", v.weight_threshold );
     visitor.add( "account_auths", v.account_auths );
@@ -163,18 +158,18 @@ public:
   template<typename StorageT>
   void add( const char* name, hive::protocol::fixed_string_impl<StorageT>& v ) const
   {
-    v = jsval[name].as<std::string>();
+    v = jsval[name].template as<std::string>();
   }
 
   template<typename SafeT>
   void add( const char* name, fc::safe<SafeT>& v ) const
   {
-    v.value = jsval[name].as<SafeT>();
+    v.value = jsval[name].template as<SafeT>();
   }
 
   void add( const char* name, std::vector<char>& v ) const
   {
-    const std::string str = jsval[name].as<std::string>();
+    const std::string str = jsval[name].template as<std::string>();
     v.resize(str.size() / 2);
     fc::from_hex(str, v.data(), str.size() / 2);
   }
@@ -182,33 +177,33 @@ public:
   template<typename TArr, size_t NArr>
   void add( const char* name, fc::array<TArr, NArr>& v ) const
   {
-    const std::string str = jsval[name].as<std::string>();
+    const std::string str = jsval[name].template as<std::string>();
     fc::from_hex(str, reinterpret_cast<char *>(&v.data[0]), NArr);
   }
 
   void add( const char* name, fc::equihash::proof& v ) const
   {
-    auto seed = fc::sha256::hash( jsval[name]["seed"].as<std::string>() );
-    auto n = jsval[name]["n"].as<uint32_t>();
-    auto k = jsval[name]["k"].as<uint32_t>();
+    auto seed = fc::sha256::hash( jsval[name]["seed"].template as<std::string>() );
+    auto n = jsval[name]["n"].template as<uint32_t>();
+    auto k = jsval[name]["k"].template as<uint32_t>();
     v = fc::equihash::proof::hash( n, k, seed );
   }
 
   void add( const char* name, hive::protocol::legacy_chain_properties& v ) const
   {
-    emscripten::val amount = jsval[name]["account_creation_fee"]["amount"];
+    ManagedObjectT amount = jsval[name]["account_creation_fee"]["amount"];
 
-    if (amount.isString())
-      v.account_creation_fee.amount = boost::lexical_cast<uint64_t>(amount.as<std::string>());
+    if (amount.is_string())
+      v.account_creation_fee.amount = boost::lexical_cast<uint64_t>(amount.template as<std::string>());
     else
-      v.account_creation_fee.amount = amount.as<uint64_t>();
-    v.maximum_block_size = jsval[name]["maximum_block_size"].as<uint32_t>();
-    v.hbd_interest_rate = jsval[name]["hbd_interest_rate"].as<uint16_t>();
+      v.account_creation_fee.amount = amount.template as<uint64_t>();
+    v.maximum_block_size = jsval[name]["maximum_block_size"].template as<uint32_t>();
+    v.hbd_interest_rate = jsval[name]["hbd_interest_rate"].template as<uint16_t>();
   }
 
   void add( const char* name, hive::protocol::pow& v ) const
   {
-    val_protocol_visitor< hive::protocol::pow > visitor{ jsval[name], v, is_protobuf };
+    val_protocol_visitor< ManagedObjectT, hive::protocol::pow > visitor{ jsval[name], v, is_protobuf };
 
     visitor.add( "worker", v.worker );
     visitor.add( "input", v.input );
@@ -218,7 +213,7 @@ public:
 
   void add( const char* name, hive::protocol::pow2_input& v ) const
   {
-    val_protocol_visitor< hive::protocol::pow2_input > visitor{ jsval[name], v, is_protobuf };
+    val_protocol_visitor< ManagedObjectT, hive::protocol::pow2_input > visitor{ jsval[name], v, is_protobuf };
 
     visitor.add( "worker_account", v.worker_account );
     visitor.add( "prev_block", v.prev_block );
@@ -227,7 +222,7 @@ public:
 
   void add( const char* name, fc::time_point_sec& v ) const
   {
-    std::string time = jsval[name].as<std::string>();
+    std::string time = jsval[name].template as<std::string>();
     if (time.empty())
       v = fc::time_point_sec::min();
     else
@@ -237,7 +232,7 @@ public:
   template<uint32_t _SYMBOL>
   void add( const char* name, hive::protocol::tiny_asset<_SYMBOL>& v ) const
   {
-    v.amount = jsval[name]["amount"].as<int64_t>();
+    v.amount = jsval[name]["amount"].template as<int64_t>();
   }
 
   template< typename... Ts >
@@ -249,43 +244,43 @@ public:
   template< typename M >
   void add_scalar( const char* name, M& v ) const
   {
-    v = jsval[name].as<M>();
+    v = jsval[name].template as<M>();
   }
 
   void add_scalar( const char* name, int64_t& v ) const
   {
-    emscripten::val _val = jsval[name];
-    if (_val.isString())
-      v = boost::lexical_cast<int64_t>(_val.as<std::string>());
+    ManagedObjectT _val = jsval[name];
+    if (_val.is_string())
+      v = boost::lexical_cast<int64_t>(_val.template as<std::string>());
     else
-      v = _val.as<int64_t>();
+      v = _val.template as<int64_t>();
   }
 
   void add_scalar( const char* name, uint64_t& v ) const
   {
-    emscripten::val _val = jsval[name];
-    if (_val.isString())
-      v = boost::lexical_cast<uint64_t>(_val.as<std::string>());
+    ManagedObjectT _val = jsval[name];
+    if (_val.is_string())
+      v = boost::lexical_cast<uint64_t>(_val.template as<std::string>());
     else
-      v = _val.as<uint64_t>();
+      v = _val.template as<uint64_t>();
   }
 
   template<typename M>
   void add_array( const char* name, boost::container::flat_set<M>& v ) const
   {
-    emscripten::val arr_val = jsval[name];
-    uint32_t arr_size = arr_val["length"].as<uint32_t>();
+    ManagedObjectT arr_val = jsval[name];
 
+    auto arr_size = arr_val.array_length();
     v.reserve(arr_size);
 
-    for (uint32_t i = 0; i < arr_size; ++i)
+    for (size_t i = 0; i < arr_size; ++i)
     {
       M item;
 
       if constexpr( std::is_same< typename fc::reflector< M >::is_defined, fc::true_type >::value )
-        fc::reflector< M >::visit( val_protocol_visitor{ arr_val[i], item, is_protobuf } );
+        fc::reflector< M >::visit( val_protocol_visitor< ManagedObjectT, M >{ arr_val[i], item, is_protobuf } );
       else
-        val_protocol_visitor< M >{ arr_val, item, is_protobuf }.add( std::to_string( i ).c_str(), item );
+        val_protocol_visitor< ManagedObjectT, M >{ arr_val, item, is_protobuf }.add( std::to_string( i ).c_str(), item );
 
       v.insert(item);
     }
@@ -294,19 +289,19 @@ public:
   template<typename M>
   void add_array( const char* name, ::flat_set_ex<M>& v ) const
   {
-    emscripten::val arr_val = jsval[name];
-    uint32_t arr_size = arr_val["length"].as<uint32_t>();
+    ManagedObjectT arr_val = jsval[name];
 
+    auto arr_size = arr_val.array_length();
     v.reserve(arr_size);
 
-    for (uint32_t i = 0; i < arr_size; ++i)
+    for (size_t i = 0; i < arr_size; ++i)
     {
       M item;
 
       if constexpr( std::is_same< typename fc::reflector< M >::is_defined, fc::true_type >::value )
-        fc::reflector< M >::visit( val_protocol_visitor{ arr_val[i], item, is_protobuf } );
+        fc::reflector< M >::visit( val_protocol_visitor< ManagedObjectT, M >{ arr_val[i], item, is_protobuf } );
       else
-        val_protocol_visitor< M >{ arr_val, item, is_protobuf }.add( std::to_string( i ).c_str(), item );
+        val_protocol_visitor< ManagedObjectT, M >{ arr_val, item, is_protobuf }.add( std::to_string( i ).c_str(), item );
 
       v.insert(item);
     }
@@ -315,20 +310,20 @@ public:
   template< typename M >
   void add_array( const char* name, M& v ) const
   {
-    emscripten::val arr_val = jsval[name];
-    uint32_t arr_size = arr_val["length"].as<uint32_t>();
+    ManagedObjectT arr_val = jsval[name];
 
+    auto arr_size = arr_val.array_length();
     v.reserve(arr_size);
 
-    for (uint32_t i = 0; i < arr_size; ++i)
+    for (size_t i = 0; i < arr_size; ++i)
     {
       using TVal = typename M::value_type;
       TVal item;
 
       if constexpr( std::is_same< typename fc::reflector< TVal >::is_defined, fc::true_type >::value )
-        fc::reflector< TVal >::visit( val_protocol_visitor< TVal >{ arr_val[i], item, is_protobuf } );
+        fc::reflector< TVal >::visit( val_protocol_visitor< ManagedObjectT, TVal >{ arr_val[i], item, is_protobuf } );
       else
-        val_protocol_visitor< TVal >{ arr_val, item, is_protobuf }.add( std::to_string( i ).c_str(), item );
+        val_protocol_visitor< ManagedObjectT, TVal >{ arr_val, item, is_protobuf }.add( std::to_string( i ).c_str(), item );
 
       v.emplace_back(item);
     }
@@ -337,47 +332,38 @@ public:
   template<typename M>
   void add_object( const char* name, boost::container::flat_map<M, hive::protocol::weight_type>& v ) const
   {
-    emscripten::val arr_val = jsval[name];
+    ManagedObjectT arr_val = jsval[name];
 
     if (is_protobuf)
     {
-      emscripten::val keys = emscripten::val::global("Object").call<emscripten::val>("keys", arr_val);
-      uint32_t count = keys["length"].as<uint32_t>();
-
-      for (uint32_t i = 0; i < count; ++i)
+      for (const auto& key : arr_val.keys())
       {
-        std::string key = keys[i].as<std::string>();
-
-        v[M{key}] = arr_val[key].as<hive::protocol::weight_type>();
+        v[M{key}] = arr_val[key].template as<hive::protocol::weight_type>();
       }
     }
     else
     {
-      uint32_t arr_size = arr_val["length"].as<uint32_t>();
+      auto arr_size = arr_val.array_length();
 
-      for (uint32_t i = 0; i < arr_size; ++i)
+      for (size_t i = 0; i < arr_size; ++i)
       {
         auto el = arr_val[i];
 
-        v[M{el[0].as<std::string>()}] = el[1].as<hive::protocol::weight_type>();
+        v[M{el[0].template as<std::string>()}] = el[1].template as<hive::protocol::weight_type>();
       }
     }
   }
 
   void add_object( const char* name, boost::container::flat_map<std::string, std::vector<char>>& v ) const
   {
-    emscripten::val arr_val = jsval[name];
+    ManagedObjectT arr_val = jsval[name];
 
     if (is_protobuf)
     {
-      emscripten::val keys = emscripten::val::global("Object").call<emscripten::val>("keys", arr_val);
-      uint32_t count = keys["length"].as<uint32_t>();
-
-      for (uint32_t i = 0; i < count; ++i)
+      for (const auto& key : arr_val.keys())
       {
-        std::string key = keys[i].as<std::string>();
         std::vector<char> value;
-        const std::string hex_value = arr_val[key].as<std::string>();
+        const std::string hex_value = arr_val[key].template as<std::string>();
         value.resize(hex_value.size() / 2);
         fc::from_hex(hex_value, value.data(), hex_value.size() / 2);
 
@@ -386,13 +372,13 @@ public:
     }
     else
     {
-      uint32_t arr_size = arr_val["length"].as<uint32_t>();
+      auto arr_size = arr_val.array_length();
 
-      for (uint32_t i = 0; i < arr_size; ++i)
+      for (size_t i = 0; i < arr_size; ++i)
       {
-        std::string key = arr_val[i][0].as<std::string>();
+        std::string key = arr_val[i][0].template as<std::string>();
         std::vector<char> value;
-        const std::string hex_value = arr_val[i][1].as<std::string>();
+        const std::string hex_value = arr_val[i][1].template as<std::string>();
         value.resize(hex_value.size() / 2);
         fc::from_hex(hex_value, value.data(), hex_value.size() / 2);
 
@@ -426,18 +412,19 @@ public:
   }
 
 private:
-  emscripten::val jsval;
+  ManagedObjectT jsval;
   bool is_protobuf;
   T& val;
 };
 
+template<typename ManagedObjectT>
 template<typename T>
-val_to_static_variant::result_type val_to_static_variant::operator()( T& v )const
+typename val_to_static_variant<ManagedObjectT>::result_type val_to_static_variant<ManagedObjectT>::operator()( T& v )const
 {
-  static_assert( !binary_view::is_hive_array< T >::value, "We currently do not support arrays in static_variants when converting from emscripten::val" );
-  static_assert( !std::is_scalar< T >::value, "We only support objects in static_variants when converting from emscripten::val" );
+  static_assert( !binary_view::is_hive_array< T >::value, "We currently do not support arrays in static_variants when converting from ManagedObjectT" );
+  static_assert( !std::is_scalar< T >::value, "We only support objects in static_variants when converting from ManagedObjectT" );
 
-  fc::reflector< T >::visit( val_protocol_visitor< T >{ jsval, v, is_protobuf } );
+  fc::reflector< T >::visit( val_protocol_visitor< ManagedObjectT, T >{ jsval, v, is_protobuf } );
 }
 
 } // namespac cpp
--- ts/wasm/src/wasm_interface.cpp
+++ ts/wasm/src/wasm_interface.cpp
@@ -20,6 +20,116 @@ using namespace emscripten;
 using manabar_fn_t = result(const int32_t, const uint32_t, const uint32_t, const uint32_t, const uint32_t, const uint32_t);
 using ext_json_asset_fn_t = json_asset(const int32_t, const int32_t)const;
 
+class emscripten_managed_object
+{
+public:
+  emscripten_managed_object()
+    : jsval(emscripten::val::undefined())
+  {}
+
+  emscripten_managed_object(emscripten::val jsval)
+    : jsval(jsval)
+  {}
+
+  emscripten_managed_object(const std::string& str)
+    : jsval(str)
+  {}
+
+  static emscripten_managed_object array(const std::vector<emscripten_managed_object>& vec)
+  {
+    std::vector<emscripten::val> items;
+    items.reserve(vec.size());
+    for (const auto& item : vec)
+      items.emplace_back(emscripten::val(item.jsval));
+
+    return emscripten_managed_object{ emscripten::val::array(items.begin(), items.end()) };
+  }
+
+  static emscripten_managed_object object()
+  {
+    return emscripten_managed_object{ emscripten::val::object() };
+  }
+
+  void set(const emscripten_managed_object& key, const emscripten_managed_object& obj)
+  {
+    jsval.set(key.jsval, obj.jsval);
+  }
+
+  void set(const char* key, const emscripten_managed_object& obj)
+  {
+    jsval.set(key, obj.jsval);
+  }
+
+  void set(const char* key, const std::string& obj)
+  {
+    jsval.set(key, obj);
+  }
+
+  emscripten_managed_object operator[](const std::string& key)const
+  {
+    return emscripten_managed_object{ jsval.operator[](key) };
+  }
+
+  emscripten_managed_object operator[](const char* key)const
+  {
+    return emscripten_managed_object{ jsval.operator[](key) };
+  }
+
+  emscripten_managed_object operator[](size_t key)const
+  {
+    return emscripten_managed_object{ jsval.operator[](key) };
+  }
+
+  emscripten_managed_object operator[](int key)const
+  {
+    return emscripten_managed_object{ jsval.operator[](key) };
+  }
+
+  bool is_undefined()const
+  {
+    return jsval.isUndefined();
+  }
+
+  bool is_string()const
+  {
+    return jsval.isString();
+  }
+
+  void del(const std::string& key)
+  {
+    jsval.delete_(key);
+  }
+
+  template<typename T>
+  T as()const
+  {
+    return jsval.as<T>();
+  }
+
+  size_t array_length()const
+  {
+    return jsval["length"].as<size_t>();
+  }
+
+  std::vector<std::string> keys()const
+  {
+    std::vector<std::string> out;
+
+    emscripten::val keys = emscripten::val::global("Object").call<emscripten::val>("keys", jsval);
+    size_t count = keys["length"].as<size_t>();
+
+    out.resize(count);
+
+    for (size_t i = 0; i < count; ++i)
+      out[i] = keys[i].as<std::string>();
+
+    return out;
+  }
+
+private:
+  emscripten::val jsval;
+};
+
 /// unfortunetely emscripten can't handle correctly C++ set -> JS Set transformation, so we have to use a vector instead.
 struct required_authority_collectionV
 {
@@ -41,7 +151,7 @@ public:
   {
     cpp::safe_exception_wrapper([&]() -> void {
       fc::reflector< hive::protocol::signed_transaction >::visit(
-        val_protocol_visitor< hive::protocol::signed_transaction >{ obj, this->_transaction, is_protobuf }
+        val_protocol_visitor< emscripten_managed_object, hive::protocol::signed_transaction >{ emscripten_managed_object{ obj }, this->_transaction, is_protobuf }
       );
     });
   }
@@ -50,7 +160,7 @@ public:
   {
     cpp::safe_exception_wrapper([&]() -> void {
       hive::protocol::operation op;
-      cpp::from_jsval(obj, op, is_protobuf);
+      cpp::from_jsval(emscripten_managed_object{obj}, op, is_protobuf);
 
       this->_transaction.operations.emplace_back(op);
     });
@@ -254,7 +364,7 @@ void tx_proto_to_api(val obj) const
 {
   cpp::safe_exception_wrapper([&]() -> void {
     fc::reflector< hive::protocol::signed_transaction >::visit(
-      to_api_visitor< hive::protocol::signed_transaction >{ obj }
+      to_api_visitor< emscripten_managed_object, hive::protocol::signed_transaction >{ emscripten_managed_object{ obj } }
     );
   });
 }
@@ -263,7 +373,7 @@ void tx_api_to_proto(val obj) const
 {
   cpp::safe_exception_wrapper([&]() -> void {
     fc::reflector< hive::protocol::signed_transaction >::visit(
-      to_proto_visitor< hive::protocol::signed_transaction >{ obj }
+      to_proto_visitor< emscripten_managed_object, hive::protocol::signed_transaction >{ emscripten_managed_object{ obj } }
     );
   });
 }
