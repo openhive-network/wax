e302f2ffec76b2187e0ce658de95b1f48d646953
Bartek Wrona <wrona@syncad.com>
2025-05-20 14:14:54 +0200

    Eliminated dependencies of CI variables from helper script

--- python/tests/run_tests.sh
+++ python/tests/run_tests.sh
@@ -6,10 +6,14 @@ TIMEOUT_PROXY_MOCK_SERVER_SECONDS="${TIMEOUT_PROXY_MOCK_SERVER_SECONDS:-30}"
 MOCK_SERVER_PORT="${MOCK_SERVER_PORT:-4000}"
 MOCK_SERVER_URL="http://localhost:${MOCK_SERVER_PORT}"
 
+WAX_DIR="${SCRIPTPATH}/../../"
+PYPROJECT_DIR="${WAX_DIR}python"
+
+
 echo -e "${TXT_BLUE}Install proxy-mock-server.ts dependencies...${TXT_CLEAR}"
-pnpm -C "$CI_PROJECT_DIR/ts" --recursive install --frozen-lockfile
+pnpm -C "${WAX_DIR}/ts" --recursive install --frozen-lockfile
 echo -e "${TXT_BLUE}Starting proxy-mock-server.ts ...${TXT_CLEAR}"
-npx tsx "${CI_PROJECT_DIR}/ts/wasm/__tests__/assets/start-proxy-mock-server.ts" "${MOCK_SERVER_PORT}" 2>&1 > "${PYPROJECT_DIR}/mock-server.log" &
+npx tsx "${WAX_DIR}/ts/wasm/__tests__/assets/start-proxy-mock-server.ts" "${MOCK_SERVER_PORT}" 2>&1 > "${PYPROJECT_DIR}/mock-server.log" &
 echo "${TXT_BLUE}Waiting for proxy server to be ready...${TXT_CLEAR}"
 timeout "$TIMEOUT_PROXY_MOCK_SERVER_SECONDS" bash -c "until curl -s $MOCK_SERVER_URL > /dev/null; do sleep 1; done"
 echo "${TXT_BLUE}Server is up!${TXT_CLEAR}"
