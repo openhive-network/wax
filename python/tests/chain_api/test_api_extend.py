77eb18d23b1529428291bfcf037d67961c2f7def
Jakub Ziebinski <jziebinski@gmail.com>
2025-08-26 10:38:23 +0000

    Correct the naming of test classes, as the previous one caused a pytest warning

--- python/tests/chain_api/test_api_extend.py
+++ python/tests/chain_api/test_api_extend.py
@@ -9,20 +9,20 @@ if TYPE_CHECKING:
     from wax.interfaces import ApiCollectionT
 
 
-class TestApi(AbstractAsyncApi):
+class MockApi(AbstractAsyncApi):
     @AbstractAsyncApi.endpoint_jsonrpc
     async def endpoint_test(self) -> str:  # type: ignore[empty-body]
         """Do nothing."""
 
 
-class TestApiCollection:
+class FirstApiCollection:
     def __init__(self) -> None:
-        self.test_api = TestApi
+        self.mock_api = MockApi
 
 
-class SecondTestApiCollection:
+class SecondApiCollection:
     def __init__(self) -> None:
-        self.second_test_api = TestApi
+        self.second_mock_api = MockApi
 
 
 def assert_have_base_apis_available(extended_chain: IHiveChainInterface[ApiCollectionT]) -> None:
@@ -32,21 +32,21 @@ def assert_have_base_apis_available(extended_chain: IHiveChainInterface[ApiColle
 
 def test_extended_and_base_api_available(remote_chain: IHiveChainInterface[ApiCollectionT]) -> None:
     # ACT
-    extended_chain = remote_chain.extends(TestApiCollection)
+    extended_chain = remote_chain.extends(FirstApiCollection)
 
     # ASSERT
-    assert hasattr(extended_chain.api, "test_api"), "Extended API should have test_api attribute."
-    assert hasattr(extended_chain.api.test_api, "endpoint_test"), "test_api should have endpoint_test method."
+    assert hasattr(extended_chain.api, "mock_api"), "Extended API should have mock_api attribute."
+    assert hasattr(extended_chain.api.mock_api, "endpoint_test"), "test_api should have endpoint_test method."
     assert_have_base_apis_available(extended_chain)
 
 
 def test_double_extend_api(remote_chain: IHiveChainInterface[ApiCollectionT]) -> None:
     # ACT
-    extended_chain = remote_chain.extends(TestApiCollection)
-    extended_chain_twice = extended_chain.extends(SecondTestApiCollection)
+    extended_chain = remote_chain.extends(FirstApiCollection)
+    extended_chain_twice = extended_chain.extends(SecondApiCollection)
 
     # ASSERT
-    assert hasattr(extended_chain_twice.api, "test_api"), "Extended API should have test_api attribute."
-    assert hasattr(extended_chain_twice.api, "second_test_api"), "Extended API should have second_test_api attribute."
+    assert hasattr(extended_chain_twice.api, "mock_api"), "Extended API should have mock_api attribute."
+    assert hasattr(extended_chain_twice.api, "second_mock_api"), "Extended API should have second_mock_api attribute."
 
     assert_have_base_apis_available(extended_chain)
