fbbb1fca45dddcc71cc5e3636536c2339f8b83bc
Jakub Ziebinski <jziebinski@gmail.com>
2025-09-05 10:48:52 +0000

    Modify the wax buil script -> remove old the dist directory if it exists

--- python/scripts/build_wax.sh
+++ python/scripts/build_wax.sh
@@ -72,6 +72,11 @@ else
   add_api_dependency "database_api" "${DATABASE_API_WHEEL_BUILD_VERSION}"
   add_api_dependency "network_broadcast_api" "${NETWORK_BROADCAST_API_WHEEL_BUILD_VERSION}"
 
+  if [ -d "${PROJECT_DIR}/dist" ]; then
+    echo "Found existing dist directory, removing it."
+    rm -rf "${PROJECT_DIR}/dist"
+  fi
+
   echo "Build wax wheel package."
   poetry -C ${PROJECT_DIR} build --format wheel
 
