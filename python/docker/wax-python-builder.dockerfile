e67036ed870e646baf8cebc21ddc4bdfd71c4950
Jakub Ziebinski <jziebinski@gmail.com>
2025-09-12 08:02:05 +0000

    Handle flattened openapi description in wax

--- python/docker/wax-python-builder.dockerfile
+++ python/docker/wax-python-builder.dockerfile
@@ -10,7 +10,14 @@ USER root
 RUN groupadd -g $GROUP_ID usergroup && \
     useradd -m -s /bin/bash -u $USER_ID -g $GROUP_ID ${USER_NAME} && \
     usermod -a -G $(id -g hived_admin) ${USER_NAME} && \
-    DEBIAN_FRONTEND=noninteractive apt update && DEBIAN_FRONTEND=noninteractive apt install -y gdb python3.12-dbg
+    DEBIAN_FRONTEND=noninteractive apt update && DEBIAN_FRONTEND=noninteractive apt install -y gdb python3.12-dbg && \
+    apt update && apt install -y curl && \
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash && \
+    export NVM_DIR="/root/.nvm" && \
+    . "$NVM_DIR/nvm.sh" && \
+    nvm install --lts && \
+    nvm use --lts
+
 
 # Switch to created user
 USER ${USER_NAME}
--- python/scripts/build_api_packages.sh
+++ python/scripts/build_api_packages.sh
@@ -4,11 +4,17 @@ set -xeuo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 WAX_PROJECT_DIR="$(dirname "$SCRIPT_DIR")/.."
-API_GENERATION_DIR="${WAX_PROJECT_DIR}/hive/libraries/plugins/apis/api_generation"
+APIS_DIR="${WAX_PROJECT_DIR}/hive/libraries/plugins/apis"
+API_GENERATION_DIR="${APIS_DIR}/api_generation"
 
 cd "${API_GENERATION_DIR}/api_generation"
 poetry install
-${API_GENERATION_DIR}/generate_api_packages.sh database_api network_broadcast_api
+
+cd "${API_GENERATION_DIR}"
+npm install @apidevtools/json-schema-ref-parser@14.2.1 json-schema-merge-allof@0.8.1  # Install dependencies for flatten_swagger.js
+node "${API_GENERATION_DIR}/flatten_swagger.js" "${APIS_DIR}/documentation/openapi.json" > "${APIS_DIR}/documentation/openapi_flattened.json"
+
+"${API_GENERATION_DIR}/generate_api_packages.sh" database_api network_broadcast_api
 
 poetry -C "${API_GENERATION_DIR}/database_api" build --format wheel
 poetry -C "${API_GENERATION_DIR}/network_broadcast_api" build --format wheel
@@ -18,3 +24,6 @@ NETWORK_BROADCAST_API_WHEEL_BUILD_VERSION=$(poetry -C "${API_GENERATION_DIR}/net
 
 echo "DATABASE_API_WHEEL_BUILD_VERSION=${DATABASE_API_WHEEL_BUILD_VERSION}" >> "${SCRIPT_DIR}/../../build_wheel.env"
 echo "NETWORK_BROADCAST_API_WHEEL_BUILD_VERSION=${NETWORK_BROADCAST_API_WHEEL_BUILD_VERSION}" >> "${SCRIPT_DIR}/../../build_wheel.env"
+
+echo "Clean up generated flattened openapi_flattened.json."
+rm -f "${APIS_DIR}/documentation/openapi_flattened.json"
