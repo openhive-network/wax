a1a4dea74372588139cff4d2c0a1a384dc5896aa
Jakub Ziebinski <jziebinski@gmail.com>
2025-09-23 12:33:11 +0000

    Fix importing transaction from network broadcast api package

--- python/wax/_private/api/models.py
+++ python/wax/_private/api/models.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from database_api.database_api_description import Active, Owner, Posting
 from database_api.database_api_description import FindAccountsResponse as FindAccountsApiResponse
-from network_broadcast_api.network_broadcast_api_description import Transaction as ApiTransaction
+from network_broadcast_api.network_broadcast_api_description import Trx as ApiTransaction
 
 PossibleAuthorityApi = Active | Owner | Posting
 
