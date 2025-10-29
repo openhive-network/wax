e6f7f16feda5f3f86652f59325a5fbbf01a97c2f
Jakub Ziebinski <jziebinski@gmail.com>
2025-10-29 13:29:43 +0000

    Fill __init__ file in the hive apps operations package

---
+++ python/wax/hive_apps_operations/__init__.py
@@ -0,0 +1,9 @@
+from .community import CommunityOperation
+from .follow import FollowOperation
+from .rc import ResourceCreditsOperation
+
+__all__ = [
+    "CommunityOperation",
+    "FollowOperation",
+    "ResourceCreditsOperation",
+]
