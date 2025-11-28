4dc3eb48fd5817d9d820f721b3fbf8878e94edb7
mtyszczak <mateusz.tyszczak@gmail.com>
2025-11-28 10:26:56 +0100

    Add LegacyApiTransaction type in TS

--- ts/wasm/lib/detailed/api/types/transaction.ts
+++ ts/wasm/lib/detailed/api/types/transaction.ts
@@ -1,13 +1,20 @@
+// We could potentially reuse types generated from wax-api-jsonrpc package here
+
 export interface ApiOperation {
   type: string;
-  value: object;
+  value: Record<string, any>;
 }
 
-export interface ApiTransaction {
+export type LegacyApiOperation = ([string, Record<string, any>]) | ([number, Record<string, any>]);
+
+interface BaseTransaction<OpType> {
   ref_block_num: number;
   ref_block_prefix: number;
   expiration: string;
-  operations: Array<ApiOperation>;
+  operations: Array<OpType>;
   extensions: object[];
   signatures: string[];
 }
+
+export type ApiTransaction = BaseTransaction<ApiOperation>;
+export type LegacyApiTransaction = BaseTransaction<LegacyApiOperation>;
