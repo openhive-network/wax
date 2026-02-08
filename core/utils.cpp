118cc41b9e3c5cabf1f0bf5691d038b26cace2f3
Bartek Wrona <bwrona@syncad.com>
2026-02-08 19:13:44 +0000

    Extract exception catch chain from safe_exception_wrapper template
    Move all exception classification logic into a non-template
    [[noreturn]] safe_exception_handler() function defined in utils.cpp.
    The template now contains only try { return fn(...); } catch (...).
    
    This avoids Emscripten duplicating the exception handling code for
    every template instantiation (~40 call sites), reducing WASM binary
    size.

--- core/utils.cpp
+++ core/utils.cpp
@@ -1,5 +1,6 @@
 #include "core/utils.hpp"
 
+#include <boost/lexical_cast.hpp>
 #include <fc/io/json.hpp>
 
 namespace cpp {
@@ -16,6 +17,49 @@ assertion_id_data_container_t init_assertion_data()
 #include "chain_assertion_hashes_wax.inl"
 
   return container;
+}
+
+[[noreturn]] void safe_exception_handler()
+{
+  std::exception_ptr eptr = std::current_exception();
+  try
+  {
+    std::rethrow_exception(eptr);
+  }
+  catch (fc::assert_exception& e)
+  {
+    WAX_EXCEPTION_WLOG("Caught fc::assert_exception: ${details}", ("details", e.to_detail_string()));
+    uint64_t unrecognized_assertion_code = throw_recognized_wax_assertion( e );
+    throw wax_unknown_assertion( unrecognized_assertion_code, e );
+  }
+  catch (fc::exception& e)
+  {
+    WAX_EXCEPTION_WLOG("Caught fc::exception: ${details}", ("details", e.to_detail_string()));
+    throw std::runtime_error(e.to_detail_string());
+  }
+  catch (const wax_private_key_leak& e)
+  {
+    /// warning until emscripten/wasm bug will be fixed do not rethrow: https://gitlab.syncad.com/hive/wax/-/issues/161#note_252570
+    /// since this exception class is quite important to propagate to client side it should be not erased
+    /// during std::exception handling
+    throw e;
+  }
+  catch (const boost::bad_lexical_cast& e)
+  {
+    /// Preserve type to avoid breaking client code that matches on this exception type.
+    throw e;
+  }
+  catch (const std::exception& e)
+  {
+    WAX_EXCEPTION_WLOG("Caught std::exception: ${details}", ("details", e.what()));
+    /// warning until emscripten/wasm bug will be fixed do not rethrow: https://gitlab.syncad.com/hive/wax/-/issues/161#note_252570
+    throw std::runtime_error(e.what());
+  }
+  catch (...)
+  {
+    WAX_EXCEPTION_WLOG("Caught nonstandard exception");
+    throw std::runtime_error("Nonstandard exception");
+  }
 }
 
 uint64_t throw_recognized_wax_assertion( fc::assert_exception& e )
--- core/utils.hpp
+++ core/utils.hpp
@@ -22,6 +22,11 @@ namespace cpp
 {
 uint64_t throw_recognized_wax_assertion( fc::assert_exception& e );
 
+/** Classifies the current exception and re-throws it as a type safely handled by Python/WASM runtimes.
+    Defined in utils.cpp to avoid duplicating exception handling code across template instantiations.
+*/
+[[noreturn]] void safe_exception_handler();
+
 /** Allows to wrap given function call into exception handler which at most throw std::exception (safely handled by Python/WASM runtimes)
 */
 template <typename ProcessorFn, typename... Args>
@@ -31,53 +36,9 @@ static decltype(auto) safe_exception_wrapper(ProcessorFn fn, Args&&... args)
   {
     return fn(std::forward<Args>(args)...);
   }
-  catch (fc::assert_exception& e)
-  {
-    WAX_EXCEPTION_WLOG("Caught fc::assert_exception: ${details}", ("details", e.to_detail_string()));
-    uint64_t unrecognized_assertion_code = throw_recognized_wax_assertion( e );
-    throw wax_unknown_assertion( unrecognized_assertion_code, e );
-  }
-  catch (fc::exception& e)
-  {
-    WAX_EXCEPTION_WLOG("Caught fc::exception: ${details}", ("details", e.to_detail_string()));
-    throw std::runtime_error(e.to_detail_string());
-  }
-  catch (const wax_private_key_leak& e)
-  {
-    /// warning until emscripten/wasm bug will be fixed do not rethrow: https://gitlab.syncad.com/hive/wax/-/issues/161#note_252570
-    /// since this exception class is quite important to propagate to client side it shoud be not erased below
-    /// during std::exception handling
-    throw e;
-  }
-  catch (const std::exception& e)
-  {
-    WAX_EXCEPTION_WLOG("Caught std::exception: ${details}", ("details", e.what()));
-    /// warning until emscripten/wasm bug will be fixed do not rethrow: https://gitlab.syncad.com/hive/wax/-/issues/161#note_252570
-    /// bad consequence: type erasure mostly not strictly needed at client side.
-    throw std::runtime_error(e.what());/// 
-  }
   catch (...)
   {
-    std::exception_ptr e = std::current_exception();
-    if(e)
-    {
-      try
-      {
-        /// give it last chance to be recognized
-        std::rethrow_exception(e);
-      }
-      /// No need to process std::exception here as it was handled above.
-      catch (...)
-      {
-        WAX_EXCEPTION_WLOG("Caught nonstandard exception");
-        throw std::runtime_error("Nonstanard exception");
-      }
-    }
-    else
-    {
-      WAX_EXCEPTION_WLOG("Caught unknown exception");
-      throw std::runtime_error("Unknown exception.");
-    }
+    safe_exception_handler();
   }
 }
 
