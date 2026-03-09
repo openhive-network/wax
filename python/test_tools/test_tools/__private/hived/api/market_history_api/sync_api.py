63d9549e053d682b7df120473d0362b9f925eafd
Michał Kudela <kudmich@syncad.com>
2026-03-09 14:17:01 +0000

    Migrate market_history_api from schemas to hiveio_api
    Replace schemas.apis import with hiveio_api and update response type
    names. Note: GetOrderBook → MarketHistoryGetOrderBookResponse (prefixed
    to avoid collision with database_api.GetOrderBookResponse).

--- python/test_tools/test_tools/__private/hived/api/market_history_api/sync_api.py
+++ python/test_tools/test_tools/__private/hived/api/market_history_api/sync_api.py
@@ -4,40 +4,40 @@ from datetime import datetime  # noqa: TCH003
 
 from beekeepy.handle.remote import AbstractSyncApi
 
-from schemas.apis import market_history_api
+from hiveio_api import market_history_api
 
 
 class MarketHistoryApi(AbstractSyncApi):
     api = AbstractSyncApi.endpoint_jsonrpc
 
     @api
-    def get_ticker(self) -> market_history_api.GetTicker:
+    def get_ticker(self) -> market_history_api.GetTickerResponse:
         raise NotImplementedError
 
     @api
-    def get_volume(self) -> market_history_api.GetVolume:
+    def get_volume(self) -> market_history_api.GetVolumeResponse:
         raise NotImplementedError
 
     @api
-    def get_order_book(self, *, limit: int = 500) -> market_history_api.GetOrderBook:
+    def get_order_book(self, *, limit: int = 500) -> market_history_api.MarketHistoryGetOrderBookResponse:
         raise NotImplementedError
 
     @api
     def get_trade_history(
         self, *, start: datetime, end: datetime, limit: int = 1000
-    ) -> market_history_api.GetTradeHistory:
+    ) -> market_history_api.GetTradeHistoryResponse:
         raise NotImplementedError
 
     @api
-    def get_recent_trades(self, *, limit: int = 1000) -> market_history_api.GetRecentTrades:
+    def get_recent_trades(self, *, limit: int = 1000) -> market_history_api.GetRecentTradesResponse:
         raise NotImplementedError
 
     @api
     def get_market_history(
         self, *, start: datetime, end: datetime, bucket_seconds: int = 0
-    ) -> market_history_api.GetMarketHistory:
+    ) -> market_history_api.GetMarketHistoryResponse:
         raise NotImplementedError
 
     @api
-    def get_market_history_buckets(self) -> market_history_api.GetMarketHistoryBuckets:
+    def get_market_history_buckets(self) -> market_history_api.GetMarketHistoryBucketsResponse:
         raise NotImplementedError
