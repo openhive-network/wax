e5240677bb941792fd0c9238072717c38d094e79
Michał Kudela <kudmich@syncad.com>
2026-03-09 14:16:39 +0000

    Migrate block_api from schemas to hiveio_api
    Replace schemas.apis import with hiveio_api and update response type
    names (GetBlockHeader → GetBlockHeaderResponse, etc.).

--- python/test_tools/test_tools/__private/hived/api/block_api/sync_api.py
+++ python/test_tools/test_tools/__private/hived/api/block_api/sync_api.py
@@ -2,20 +2,20 @@ from __future__ import annotations
 
 from beekeepy.handle.remote import AbstractSyncApi
 
-from schemas.apis import block_api
+from hiveio_api import block_api
 
 
 class BlockApi(AbstractSyncApi):
     api = AbstractSyncApi.endpoint_jsonrpc
 
     @api
-    def get_block_header(self, *, block_num: int) -> block_api.GetBlockHeader:
+    def get_block_header(self, *, block_num: int) -> block_api.GetBlockHeaderResponse:
         raise NotImplementedError
 
     @api
-    def get_block(self, *, block_num: int) -> block_api.GetBlock:
+    def get_block(self, *, block_num: int) -> block_api.GetBlockResponse:
         raise NotImplementedError
 
     @api
-    def get_block_range(self, starting_block_num: int, count: int) -> block_api.GetBlockRange:
+    def get_block_range(self, starting_block_num: int, count: int) -> block_api.GetBlockRangeResponse:
         raise NotImplementedError
